ee11a5a7c451f630e47503cbf0367565b793b196
Diego Alexandre <ddsa.1988@outlook.com>
2023-11-17 08:26:03 -0300

    commit

--- UdemyCourse/Section013_WorkingWithFiles/Section013_WorkingWithFiles/Program.cs
+++ UdemyCourse/Section013_WorkingWithFiles/Section013_WorkingWithFiles/Program.cs
@@ -6,6 +6,8 @@ namespace Section013_WorkingWithFiles;
 public class Program {
     public static void Main(string[] args) {
 
+        CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+
         string separator = Path.AltDirectorySeparatorChar.ToString();
         string sourcePath = @$"..{separator}..{separator}..{separator}Files{separator}Product.csv";
         string targetPath = @$"..{separator}..{separator}..{separator}Files{separator}Summary.csv";
@@ -23,11 +25,11 @@ public class Program {
 
                 string[] data = line.Split(",");
                 string name = data[0];
-                float price = float.Parse(data[1], CultureInfo.InvariantCulture);
+                float price = float.Parse(data[1], cultureInfo);
                 int quantity = int.Parse((data[2]));
 
                 Product product = new Product(name, price, quantity);
-                string dataToWrite = $"{product.Name},{product.Total().ToString("F2", CultureInfo.InvariantCulture)}";
+                string dataToWrite = $"{product.Name},{product.Total().ToString("F2", cultureInfo)}";
                 sw.WriteLine(dataToWrite);
             }
         } catch (Exception e) {
