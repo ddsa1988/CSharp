b925e50d05934640c6bbc9572df176d65d45f8fc
Diego Alexandre <ddsa.1988@outlook.com>
2023-11-21 14:35:02 -0300

    commit

---
+++ UdemyCourse/Section015_GetHashCodeAndEquals/Section015_GetHashCodeAndEquals/Entities/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section015_GetHashCodeAndEquals.Entities;
+
+public class Client {
+
+    public string Name { get; set; } = "";
+    public string Email { get; set; } = "";
+    public int Id { get; set; }
+
+    public Client() {}
+
+    public Client(string name, string email, int id) {
+        Name = name;
+        Email = email;
+        Id = id;
+    }
+
+    public override bool Equals(object? obj) {
+
+        if(obj is not Client) {
+            return false;
+        }
+
+        Client? other = obj as Client;
+
+        return Id.Equals(other?.Id);
+    }
+
+    public override int GetHashCode() {
+        return Id.GetHashCode();
+    }
+}
---
+++ UdemyCourse/Section015_GetHashCodeAndEquals/Section015_GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,16 @@
+﻿using Section015_GetHashCodeAndEquals.Entities;
+
+namespace Section015_GetHashCodeAndEquals;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        Client client1 = new Client("Diego", "diego@gmail", 123);
+        Client client2 = new Client("Amanda", "amanda@gmail", 123);
+
+        Console.WriteLine(client1 == client2);
+        Console.WriteLine(client1.Equals(client2));
+        Console.WriteLine(client1.GetHashCode());
+        Console.WriteLine(client2.GetHashCode());
+    }
+}
