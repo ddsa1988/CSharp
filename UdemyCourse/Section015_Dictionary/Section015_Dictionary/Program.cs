42b778feb3873f6c42cea90ff767485f53eaf243
Diego Alexandre <ddsa.1988@outlook.com>
2023-11-22 14:39:31 -0300

    commit

---
+++ UdemyCourse/Section015_Dictionary/Section015_Dictionary/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Section015_Dictionary;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        char separator = Path.AltDirectorySeparatorChar;
+        string sourcePath = @$"..{separator}..{separator}..{separator}/Files{separator}Votes.txt";
+
+        if (File.Exists(sourcePath)) {
+
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+            candidates.Add("diego", 1000);
+            PrintCollection(candidates);
+
+            try {
+                using StreamReader sr = new StreamReader(sourcePath);
+
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    public static void PrintCollection<T>(IEnumerable<T> collection) {
+
+        foreach (T item in collection) {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static bool IsDataValid(string text) {
+
+        if(string.IsNullOrEmpty(text) || string.IsNullOrWhiteSpace(text)) return false;
+
+        if (!text.Contains(',')) return false;
+
+        try {
+            string[] textParts = text.Split(",");
+
+            return true;
+
+        } catch (Exception e) {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+    }
+}
