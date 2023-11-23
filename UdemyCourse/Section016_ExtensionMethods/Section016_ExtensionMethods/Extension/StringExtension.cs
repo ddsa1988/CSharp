69a8980c0aed4f6c429b0e2292ed8c0fad833cfb
Diego Alexandre <ddsa.1988@outlook.com>
2023-11-23 07:09:43 -0300

    commit

--- UdemyCourse/Section016_ExtensionMethods/Section016_ExtensionMethods/Extension/StringExtension.cs
+++ UdemyCourse/Section016_ExtensionMethods/Section016_ExtensionMethods/Extension/StringExtension.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace Section016_ExtensionMethods.Extension; 
 
@@ -14,5 +15,24 @@ public static class StringExtension {
         newStr = string.Concat(newStr, "...");
         
         return newStr;
+    }
+
+    public static string ToTitleCase(this string thisObj) {
+
+        if (string.IsNullOrEmpty(thisObj) || string.IsNullOrWhiteSpace(thisObj) || thisObj.Length == 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        string[] textParts = thisObj.Split(' ');
+
+        for (int i = 0; i < textParts.Length; i++) {
+            string text = textParts[i];
+
+            sb.Append(text[0].ToString().ToUpper());
+            sb.Append(text.ToLower().Substring(1) + " ");
+        }
+
+        return sb.ToString();
     }
 }
--- UdemyCourse/Section016_ExtensionMethods/Section016_ExtensionMethods/Program.cs
+++ UdemyCourse/Section016_ExtensionMethods/Section016_ExtensionMethods/Program.cs
@@ -15,5 +15,17 @@ public class Program {
         const string str1 = "Good morning dear students!";
         Console.WriteLine(str1.Cut(50));
         Console.WriteLine(str1.Cut(10));
+
+        Console.WriteLine();
+
+        string name1 = "dIEGO dos santos alexandre";
+        string? name2 = null;
+        string name3 = "dIeGo";
+        string name4 = "dIEGOdossANtosAlexandre";
+
+        Console.WriteLine(name1.ToTitleCase());
+        Console.WriteLine(name2?.ToTitleCase());
+        Console.WriteLine(name3.ToTitleCase());
+        Console.WriteLine(name4.ToTitleCase());
     }
 }
