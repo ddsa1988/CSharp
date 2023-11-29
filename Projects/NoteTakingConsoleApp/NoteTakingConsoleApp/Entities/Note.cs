f54514de8f2abfa88963815ef0611b71b5292168
Diego Alexandre <ddsa.1988@outlook.com>
2023-11-29 16:05:23 -0300

    commit

---
+++ Projects/NoteTakingConsoleApp/NoteTakingConsoleApp/Entities/Note.cs
@@ -0,0 +1,17 @@
+﻿namespace NoteTakingConsoleApp.Entities;
+
+public class Note {
+    private static int counter = 0; 
+    public int Id { get; private set; }
+    public string Text { get; set; } = "";
+
+    public Note(string text) {
+        Id = ++counter;
+        Text = text;
+    }
+
+    public override string ToString() {
+        return "Id: " + Id +
+            " Text: " + Text;
+    }
+}
---
+++ Projects/NoteTakingConsoleApp/NoteTakingConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using NoteTakingConsoleApp.Entities;
+
+namespace NoteTakingConsoleApp;
+
+public class Program {
+    public static void Main(string[] args) {
+        char separator = Path.AltDirectorySeparatorChar;
+
+        string sourcePath = @$"..{separator}..{separator}..{separator}Files{separator}Text.txt";
+        Console.WriteLine(Path.Exists(sourcePath));
+
+        string text = File.ReadAllText(sourcePath);
+        Console.WriteLine(text + "\n");
+
+        Note n1 = new Note("test1");
+        Note n2 = new Note("test2");
+        Note n3 = new Note("test3");
+
+        Console.WriteLine(n1);
+        Console.WriteLine(n2);
+        Console.WriteLine(n3);
+
+    }
+}
