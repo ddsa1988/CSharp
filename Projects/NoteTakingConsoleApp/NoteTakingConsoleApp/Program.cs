c15e985d3a9106d95fe3d060b61d5a69a97f1e2d
Diego Alexandre <ddsa.1988@outlook.com>
2023-12-05 08:52:21 -0300

    commit

---
+++ Projects/NoteTakingConsoleApp/NoteTakingConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using NoteTakingConsoleApp.Entities;
+
+namespace NoteTakingConsoleApp;
+
+public class Program {
+    public static void Main(string[] args) {
+        char separator = Path.AltDirectorySeparatorChar;
+
+        string sourcePath = @$"..{separator}..{separator}..{separator}Files{separator}DataBase.txt";
+
+        Notebook notebook = new Notebook(sourcePath);
+        Console.WriteLine(notebook);
+
+        notebook.RemoveNote(2);
+    }
+}
