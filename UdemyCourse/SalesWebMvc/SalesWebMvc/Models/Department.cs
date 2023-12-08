c33ee6ae3c29fce06e44efdd7b32d554904dcabc
Diego Alexandre <ddsa.1988@outlook.com>
2023-12-07 22:19:52 -0300

    commit

---
+++ UdemyCourse/SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -0,0 +1,18 @@
+namespace SalesWebMvc.Models;
+
+public class Department {
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+
+    public Department() { }
+
+    public Department(int id, string name) {
+        Id = id;
+        Name = name;
+    }
+
+    public override string ToString() {
+        return "Id = " + Id +
+               " Name = " + Name;
+    }
+}
