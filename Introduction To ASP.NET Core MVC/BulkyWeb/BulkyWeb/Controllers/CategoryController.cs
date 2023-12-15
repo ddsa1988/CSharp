71d611bf1c357f8798ecc06a9bbf870558c21d9e
Diego Alexandre <ddsa.1988@outlook.com>
2023-12-14 22:48:28 -0300

    commit

--- Introduction To ASP.NET Core MVC/BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
+++ Introduction To ASP.NET Core MVC/BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
@@ -26,6 +26,7 @@ public class CategoryController : Controller {
 
         db.Categories.Add(category);
         db.SaveChanges();
+
         return RedirectToAction("Index");
     }
 
@@ -51,6 +52,33 @@ public class CategoryController : Controller {
         if (!ModelState.IsValid) return View();
 
         db.Categories.Update(category);
+        db.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int? id) {
+        if (id == null || id == 0) {
+            return NotFound();
+        }
+
+        Category? category = db.Categories.Find(id);
+        // Category? category = db.Categories.FirstOrDefault(cat => cat.Id == id);
+        // Category? category = db.Categories.Where(cat => cat.Id == id).FirstOrDefault();
+
+        if (category == null) {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Category category) {
+        db.Categories.Remove(category);
+        db.SaveChanges();
+
         return RedirectToAction("Index");
     }
 }
