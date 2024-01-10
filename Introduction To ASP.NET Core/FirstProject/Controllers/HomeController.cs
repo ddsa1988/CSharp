602a6bf3c30372f075ceb61933b6158dd9c624a0
Diego Alexandre <ddsa.1988@outlook.com>
2024-01-09 23:18:30 -0300

    commit

--- Introduction To ASP.NET Core/FirstProject/Controllers/HomeController.cs
+++ Introduction To ASP.NET Core/FirstProject/Controllers/HomeController.cs
@@ -4,10 +4,12 @@ namespace FirstProject.Controllers;
 
 public class HomeController : Controller {
     public ViewResult Index() {
-        int hour = DateTime.Now.Hour;
-        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        Dictionary<string, int> people = new Dictionary<string, int>();
+        people.Add("Diego", 35);
+        people.Add("Amanda", 30);
+        people.Add("Rodrigo", 29);
 
-        return View("MyView", viewModel);
+        return View("MyView", people);
     }
 
     public IActionResult Privacy() {
