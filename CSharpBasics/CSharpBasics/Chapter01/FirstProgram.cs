e5686c7496aa14286e145312ccedb5b00f9b927c
Diego Alexandre <ddsa.1988@outlook.com>
2024-02-13 21:34:08 -0300

    commit

---
+++ CSharpBasics/CSharpBasics/Chapter01/FirstProgram.cs
@@ -0,0 +1,7 @@
+namespace CSharpBasics.Chapter01;
+
+public class FirstProgram {
+    public static void MyMain() {
+        Console.WriteLine("Main");
+    }
+}
---
+++ CSharpBasics/CSharpBasics/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace CSharpBasics;
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("CSharp basics");
+    }
+}
