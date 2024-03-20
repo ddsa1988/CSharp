688d0409cb930426d6fe91e6a142aefb02ea6bbc
Diego Alexandre <ddsa.1988@outlook.com>
2024-03-19 22:27:40 -0300

    commit

--- CSharpBasics/CSharpBasics/Chapter02/Inheritance.cs
+++ CSharpBasics/CSharpBasics/Chapter02/Inheritance.cs
@@ -4,8 +4,14 @@ namespace CSharpBasics.Chapter02;
 
 public class Inheritance {
     public static void MyMain() {
-        Asset a1 = new Asset() { Name = "Test" };
+        Asset a1 = new Asset() { Name = "Test1" };
         Console.WriteLine(a1);
+
+        Stock s1 = new Stock("Test2", 600);
+        Console.WriteLine(s1);
+        
+        House m1 = new House("Test3", 1200);
+        Console.WriteLine(m1);
     }
 
     private class Asset {
@@ -24,6 +30,44 @@ public class Inheritance {
 
         public override string ToString() {
             return "Name: " + Name;
+        }
+    }
+
+    private class Stock : Asset {
+        private int sharedOwned;
+
+        public Stock() { }
+
+        public Stock(string name, int sharedOwned) : base(name) {
+            SharedOwned = sharedOwned;
+        }
+
+        public int SharedOwned {
+            get => sharedOwned;
+            set => sharedOwned = value > 0 ? value : 0;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $", Shared owned: {SharedOwned:F2}";
+        }
+    }
+
+    private class House : Asset {
+        private double mortgage;
+
+        public House() { }
+
+        public House(string name, double mortgage) : base(name) {
+            Mortgage = mortgage;
+        }
+
+        public double Mortgage {
+            get => mortgage;
+            set => mortgage = value > 0 ? value : 0;
+        }
+
+        public override string ToString() {
+            return base.ToString() + $", Mortgage: {Mortgage:C2}";
         }
     }
 }
