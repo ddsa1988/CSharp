d4f7edb546ff3775b0d7959c5132fdf605e00f10
Diego Alexandre <ddsa.1988@outlook.com>
2025-01-28 23:41:10 -0300

    commit

--- Basics/Basics/S011_ObjectOrientedProgramming/Models/Car.cs
+++ Basics/Basics/S011_ObjectOrientedProgramming/Models/Car.cs
@@ -1,20 +1,19 @@
 namespace Basics.S011_ObjectOrientedProgramming.Models;
 
 public class Car {
+    // Default custom constructors
+    public Car() : this(string.Empty, 0) { }
+    public Car(string petName) : this(petName, 0) { }
+    public Car(int currentSpeed) : this(string.Empty, currentSpeed) { }
+
+    public Car(string petName, int currentSpeed) {
+        PetName = petName;
+        CurrentSpeed = currentSpeed;
+    }
+
     // The 'state' of the Car
     public string? PetName { get; set; }
     public int CurrentSpeed { get; set; }
-
-    // Default custom constructors
-    public Car() : this("string.Empty") { }
-
-    public Car(string petName) {
-        PetName = petName;
-    }
-
-    public Car(string petName, int currentSpeed) : this(petName) {
-        CurrentSpeed = currentSpeed;
-    }
 
     // The 'behaviors' of the Car
     public void PrintState() {
