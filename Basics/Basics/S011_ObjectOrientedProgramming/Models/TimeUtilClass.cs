b7a39685888b2fe70cdb1673d86b5b8825961871
Diego Alexandre <ddsa.1988@outlook.com>
2025-02-06 18:35:15 -0300

    commit

--- Basics/Basics/S011_ObjectOrientedProgramming/Models/TimeUtilClass.cs
+++ Basics/Basics/S011_ObjectOrientedProgramming/Models/TimeUtilClass.cs
@@ -1,4 +1,5 @@
 ﻿namespace Basics.S011_ObjectOrientedProgramming.Models;
+
 public static class TimeUtilClass {
     public static void PrintTime() {
         Console.WriteLine("Actual time: " + DateTime.Now.ToShortTimeString());
@@ -9,14 +10,14 @@ public static class TimeUtilClass {
     }
 
     public static int GetAge(DateTime birthday) {
-        const int DAYS_PER_YEAR = 365;
+        const int daysPerYear = 365;
 
         DateTime now = DateTime.Now;
         TimeSpan elapsed = now.Subtract(birthday);
 
         ArgumentOutOfRangeException.ThrowIfLessThan(elapsed.TotalDays, 0, nameof(birthday));
 
-        int age = (int)(elapsed.TotalDays / DAYS_PER_YEAR);
+        int age = (int)(elapsed.TotalDays / daysPerYear);
 
         return age;
     }
