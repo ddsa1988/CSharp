4b84081d6051029d4da36d8df84c083de757f609
Diego Alexandre <ddsa.1988@outlook.com>
2025-02-19 00:32:09 -0300

    commit

---
+++ AspNet Core/Basics/S001_FirstProject/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace S001_FirstProject.Models;
+
+public class ErrorViewModel {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
