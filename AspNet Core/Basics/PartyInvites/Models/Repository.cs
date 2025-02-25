3244901fdffe8bd99d4462169542cd776613ac66
Diego Alexandre <ddsa.1988@outlook.com>
2025-02-24 23:33:49 -0300

    commit

--- AspNet Core/Basics/PartyInvites/Models/Repository.cs
+++ AspNet Core/Basics/PartyInvites/Models/Repository.cs
@@ -6,6 +6,8 @@ public static class Repository {
     public static IEnumerable<GuestResponse> Responses => _responses;
     
     public static void AddResponse(GuestResponse response) {
+        if(_responses.Contains(response)) return;
+        
         _responses.Add(response);
     }
 }
