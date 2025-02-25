07ed562d2fd3f53ac02b608563d737f796120321
Diego Alexandre <d.alexandre@acxautomation.com>
2025-02-25 13:14:05 -0300

    commit

--- AspNet Core/Basics/PartyInvites/Models/Repository.cs
+++ AspNet Core/Basics/PartyInvites/Models/Repository.cs
@@ -4,10 +4,10 @@ public static class Repository {
     private static List<GuestResponse> _responses = [];
 
     public static IEnumerable<GuestResponse> Responses => _responses;
-    
+
     public static void AddResponse(GuestResponse response) {
-        if(_responses.Contains(response)) return;
-        
+        if (_responses.Contains(response)) return;
+
         _responses.Add(response);
     }
 }
