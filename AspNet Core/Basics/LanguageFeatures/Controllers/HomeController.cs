16d487ba9b8b4009ce7a01cffd387f86f52267ec
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-03 14:19:53 -0300

    commit

---
+++ AspNet Core/Basics/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers;
+
+public class HomeController : Controller {
+    public ViewResult Index() {
+        return View(new string[] { "C#", "Language", "Features" });
+    }
+}
---
+++ AspNet Core/Basics/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace LanguageFeatures.Models;
+
+public class Product {
+    public string Name { get; set; }
+    public double? Price { get; set; }
+
+    public static Product[] GetProducts() {
+        Product kayak = new Product() { Name = "Kayak", Price = 275 };
+        Product lifeJacket = new Product() { Name = "Lifejacket", Price = 48.95 };
+
+        return new Product[] { kayak, lifeJacket, null };
+    }
+}
