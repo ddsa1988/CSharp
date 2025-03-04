4acce25106318cad769d0dbd37010df83f624307
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-04 15:10:38 -0300

    commit

---
+++ AspNet Core/Basics/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿namespace LanguageFeatures.Models;
+
+public static class MyExtensionMethods {
+    public static double TotalPrice(this ShoppingCart cartParam) {
+        double totalPrice = 0;
+
+        if (cartParam == null) return totalPrice;
+
+        foreach (Product? prod in cartParam.Products) {
+            totalPrice += prod?.Price ?? 0;
+        }
+
+        return totalPrice;
+    }
+}
