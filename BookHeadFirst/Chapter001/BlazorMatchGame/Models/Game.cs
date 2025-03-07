7da7ec44d8821181420b9305f7a08711d59828e5
Diego Alexandre <ddsa.1988@outlook.com>
2025-03-07 01:08:04 -0300

    commit

--- BookHeadFirst/Chapter001/BlazorMatchGame/Models/Game.cs
+++ BookHeadFirst/Chapter001/BlazorMatchGame/Models/Game.cs
@@ -1,7 +1,8 @@
 namespace BlazorMatchGame.Models;
 
 public static class Game {
-    private static string _lastEmoji = string.Empty;    
+    private static string _lastEmoji = string.Empty;
+    private static string _lastButton = string.Empty;
     public static List<string> ShuffledEmojis { get; private set; }
 
     static Game() {
@@ -12,19 +13,17 @@ public static class Game {
         ShuffledEmojis = Emoji.GetEmojis().OrderBy(item => new Random().Next()).ToList();
     }
 
-    public static void ButtonClick(string emoji) {
-        if (_lastEmoji == string.Empty) {
-            _lastEmoji = emoji;
+    public static void ButtonClick(string emoji, string button) {
+        if (emoji != string.Empty && _lastEmoji == emoji && _lastButton != button) {
+            _lastEmoji = string.Empty;
+            _lastButton = string.Empty;
+
+            ShuffledEmojis = ShuffledEmojis.Select(item => item.Replace(emoji, string.Empty)).ToList();
+
             return;
         }
 
-        if (_lastEmoji == emoji) {
-            _lastEmoji = string.Empty;
-            ShuffledEmojis = ShuffledEmojis.Select(item => item.Replace(emoji, string.Empty)).ToList();
-            
-            return;
-        }
-        
-        _lastEmoji = string.Empty;
+        _lastEmoji = emoji;
+        _lastButton = button;
     }
 }
