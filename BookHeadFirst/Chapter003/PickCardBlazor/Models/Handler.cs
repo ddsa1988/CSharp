9ec5a20fd6b88b93b790bc987a37b091428a7a8b
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-11 14:32:56 -0300

    commit

---
+++ BookHeadFirst/Chapter003/PickCardBlazor/Models/Handler.cs
@@ -0,0 +1,15 @@
+﻿namespace PickCardBlazor.Models;
+
+public static class Handler {
+    public static int NumberOfCards { get; set; }
+    public static string[] PickedCards { get; private set; }
+
+    static Handler() {
+        NumberOfCards = 5;
+        PickedCards = [];
+    }
+
+    public static void UpdateCards() {
+        PickedCards = CardPicker.PickSomeCards(NumberOfCards);
+    }
+}
