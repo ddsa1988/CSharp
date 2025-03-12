06f9a26bc8f6716545f4d21c903a6d38b535ed47
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-12 13:28:01 -0300

    commit

---
+++ BookHeadFirst/Chapter003/SimpleCassinoBlazor/Program.cs
@@ -0,0 +1,23 @@
+using SimpleCassinoBlazor.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
