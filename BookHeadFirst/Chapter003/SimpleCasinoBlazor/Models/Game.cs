a6a9357518d27425512a90d19f8aba0f466ba9a9
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-14 12:18:54 -0300

    commit

--- BookHeadFirst/Chapter003/SimpleCasinoBlazor/Models/Game.cs
+++ BookHeadFirst/Chapter003/SimpleCasinoBlazor/Models/Game.cs
@@ -5,6 +5,7 @@ public static class Game {
     private static readonly double _betMultiply;
     public static double Odds { get; private set; }
     public static double BetValue { get; set; }
+    public static double DepositValue { get; set; }
     public static string TextResult { get; set; }
     public static Account Player { get; set; }
 
@@ -24,6 +25,16 @@ public static class Game {
         BetValue = 0;
     }
 
+    public static void Deposit(double value) {
+        if (value <= 0) {
+            TextResult = "The deposit value must be greater than zero!";
+            return;
+        }
+
+        Player.Balance += value;
+        TextResult = $"You deposit {value:C2}";
+    }
+
     public static void Bet(double value) {
         if (value <= 0) {
             TextResult = "The bet value must be greater than zero.";
@@ -36,9 +47,16 @@ public static class Game {
         }
 
         double randomValue = _random.NextDouble();
-        double pot = value * 2;
+        double pot = value * _betMultiply;
 
-        Console.WriteLine(randomValue);
-        Console.WriteLine(value);
+        Player.Balance -= value;
+
+        if (randomValue > Odds) {
+            Player.Balance += pot;
+            TextResult = $"You won {pot:C2}\n";
+        } else {
+            TextResult = "Bad luck, you lost.";
+            TextResult += Player.Balance <= 0 ? " The house always wins!" : "";
+        }
     }
 }
