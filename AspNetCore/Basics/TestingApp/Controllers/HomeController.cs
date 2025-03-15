75868b1ba0832e3fb4bb7d3d96425da42afde2f6
Diego Alexandre <d.alexandre@acxautomation.com>
2025-03-15 11:09:57 -0300

    commit

---
+++ AspNetCore/Basics/TestingApp/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestingApp.Models;
+
+namespace TestingApp.Controllers;
+
+public class HomeController : Controller {
+    public ViewResult Index() {
+        return View(Product.GetProducts());
+    }
+}
---
+++ AspNetCore/Basics/TestingApp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace TestingApp.Models;
+
+public class Product {
+    public string? Name { get; set; } = string.Empty;
+    public decimal? Price { get; set; }
+
+    public static Product[] GetProducts() {
+        Product kayak = new Product() { Name = "Kayak", Price = 275M };
+        Product lifeJacket = new Product() { Name = "Life Jacket", Price = 48.85M };
+
+        return [kayak, lifeJacket];
+    }
+}
