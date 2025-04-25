94fd8d8c9349c1abc25b8287e055597368074f2e
Diego Alexandre <d.alexandre@acxautomation.com>
2025-04-24 23:32:50 -0300

    commit

--- AspNetTutorial/GameStore/Program.cs
+++ AspNetTutorial/GameStore/Program.cs
@@ -1,6 +1,15 @@
+using GameStore.Dto;
+using GameStore.Utils;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = Games.GetGames();
+
+// Get /games
+app.MapGet("games", () => games);
+
+// Get /games/id
+app.MapGet("games/{id:int}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
