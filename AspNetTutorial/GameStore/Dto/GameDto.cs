fff2860d139ef46d61703def95765e331a3e24f9
Diego Alexandre <ddsa.1988@outlook.com>
2025-04-26 01:28:36 -0300

    commit

--- AspNetTutorial/GameStore/Dto/GameDto.cs
+++ AspNetTutorial/GameStore/Dto/GameDto.cs
@@ -1,3 +1,9 @@
 ﻿namespace GameStore.Dto;
 
-public record GameDto(int Id, string Name, string Genre, float Price, DateOnly ReleaseDate);
+public record GameDto() {
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string Genre { get; init; } = string.Empty;
+    public float Price { get; init; }
+    public DateOnly ReleaseDate { get; init; }
+}
