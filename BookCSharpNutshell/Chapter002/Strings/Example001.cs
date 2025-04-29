a9b11e7ee01136f620e939053c9f4ee08bf591f5
Diego Alexandre <d.alexandre@acxautomation.com>
2025-04-29 15:45:23 -0300

    commit

--- BookCSharpNutshell/Chapter002/Strings/Example001.cs
+++ BookCSharpNutshell/Chapter002/Strings/Example001.cs
@@ -1,5 +1,11 @@
 namespace Chapter002.Strings;
 
 public static class Example001 {
-    public static void UserMain() { }
+    public static void UserMain() {
+        // The C# char type represents a Unicode character and occupies 2 bytes
+
+        const char a = 'A';
+
+        Console.WriteLine("{0} = {1}", nameof(a), a);
+    }
 }
---
+++ BookCSharpNutshell/Chapter002/Strings/Example002.cs
@@ -0,0 +1,8 @@
+﻿namespace Chapter002.Strings;
+
+public static class Example002 {
+    public static void UserMain() {
+        // Escape sequences express characters that cannot be expressed or interpreted literally.
+        // An escape sequence is a backslash followed by a character with a special meaning.
+    }
+}
