13222edc8b0c5fabf5625f795ad72f72f5d554fe
Diego Alexandre <ddsa.1988@outlook.com>
2025-05-02 23:46:57 -0300

    commit

--- BookCSharpNutshell/Chapter002/Arrays/Example003.cs
+++ BookCSharpNutshell/Chapter002/Arrays/Example003.cs
@@ -2,6 +2,21 @@
 
 public static class Example003 {
     public static void UserMain() {
-        // Indices
+        // Indices let you refer to elements relative to the end of an array, with the ^ operator.
+        // ^1 refers to the last element, ^2 refers to the second-to-last element, and so on
+
+        char[] vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        Console.WriteLine(vowels[0]);
+        Console.WriteLine(vowels[^1]);
+
+        Console.WriteLine(vowels[1]);
+        Console.WriteLine(vowels[^2]);
+
+        Index first = 0;
+        Index last = ^1;
+
+        Console.WriteLine(vowels[first]);
+        Console.WriteLine(vowels[last]);
     }
 }
---
+++ BookCSharpNutshell/Chapter002/Arrays/Example004.cs
@@ -0,0 +1,17 @@
+﻿namespace Chapter002.Arrays;
+
+public static class Example004 {
+    public static void UserMain() {
+        // Ranges let you “slice” an array by using the .. operator
+
+        char[] vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        char[] firstTwo = vowels[..2];
+        char[] lastThree = vowels[2..];
+        char[] middleOne = vowels[2..3];
+
+        Console.WriteLine(string.Join(" ", firstTwo));
+        Console.WriteLine(string.Join(" ", lastThree));
+        Console.WriteLine(string.Join(" ", middleOne));
+    }
+}
