831c68c1c2d9cb9f3b2ee73ec86ca2da9050b303
Diego Alexandre <ddsa.1988@outlook.com>
2025-05-13 22:58:30 -0300

    commit

--- BookAspnetCore/FirstApp/Controllers/HomeController.cs
+++ BookAspnetCore/FirstApp/Controllers/HomeController.cs
@@ -4,28 +4,8 @@ using FirstApp.Models;
 
 namespace FirstApp.Controllers;
 
-public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+public class HomeController : Controller {
+    public string Index() {
+        return "Hello, I'm Diego alexandre.";
     }
 }
