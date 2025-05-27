d9e70f5036e0a3febe4c462f2c8c29b68f4ee33a
Diego Alexandre <ddsa.1988@outlook.com>
2025-05-26 22:41:15 -0300

    commit

--- AspnetCoreTutorial/BookStore/Dto/Book/UpdateBookDto.cs
+++ AspnetCoreTutorial/BookStore/Dto/Book/UpdateBookDto.cs
@@ -4,8 +4,8 @@ namespace BookStore.Dto.Book;
 
 public record class UpdateBookDto(
     [Required] [StringLength(30)] string Title,
-    [Required] [StringLength(30)] string Author,
-    [Required] [StringLength(30)] string Publisher,
+    [Required] [Range(1, int.MaxValue)] int AuthorId,
+    [Required] [Range(1, int.MaxValue)] int PublisherId,
     [Required] [StringLength(20)] string Isbn,
     [Required] [Range(1, int.MaxValue)] int Edition,
     [Required] [Range(1, float.MaxValue)] float Price,
--- AspnetCoreTutorial/BookStore/Endpoints/BooksEndpoints.cs
+++ AspnetCoreTutorial/BookStore/Endpoints/BooksEndpoints.cs
@@ -1,4 +1,8 @@
+using BookStore.Data;
 using BookStore.Dto.Book;
+using BookStore.Entities;
+using BookStore.Mapping;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Endpoints;
 
@@ -12,54 +16,77 @@ public static class BooksEndpoints {
         RouteGroupBuilder group = app.MapGroup("books").WithParameterValidation();
 
         // Get /books
-        group.MapGet("/", () => Books);
+        group.MapGet("/", async (BookStoreContext dbContext) => {
+            return await dbContext.Book
+                .Include(book => book.Author)
+                .Include(book => book.Publisher)
+                .Select(book => book.ToBookSummaryDto())
+                .AsNoTracking()
+                .ToListAsync();
+        });
 
         // Get /books/id
-        group.MapGet("/{id:int}", (int id) => {
-            BookSummaryDto? book = Books.Find(book => book.Id == id);
+        group.MapGet("/{id:int}", async (int id, BookStoreContext dbContext) => {
+            Book? book = await dbContext.Book.FindAsync(id);
 
-            return book == null ? Results.NotFound() : Results.Ok(book);
+            return book == null ? Results.NotFound() : Results.Ok(book.ToBookDetailsDto());
         }).WithName(GetBookEndpoint);
 
         // Post /books
-        group.MapPost("/", (CreateBookDto newBook) => {
+        group.MapPost("/", async (CreateBookDto newBook, BookStoreContext dbContext) => {
+            Book? existingBook = await dbContext.Book.FirstOrDefaultAsync(book => book.Title == newBook.Title);
+
+            if (existingBook != null) return Results.Conflict("Book already exists");
+
+            Author? existingAuthor = await dbContext.Author.FindAsync(newBook.AuthorId);
+
+            if (existingAuthor == null) return Results.NotFound("Author not found");
+
+            Publisher? existingPublisher = await dbContext.Publisher.FindAsync(newBook.PublisherId);
+
+            if (existingPublisher == null) return Results.NotFound("Publisher not found");
+
             int index = Books.FindIndex(book => book.Title == newBook.Title);
 
             if (index != -1) return Results.Conflict("Book already exists");
 
-            var book = new BookSummaryDto(_nextId++, newBook.Title, newBook.Author, newBook.Publisher, newBook.Isbn,
-                newBook.Edition,
-                newBook.Price, newBook.PublishDate);
+            Book book = newBook.ToEntity(existingAuthor, existingPublisher);
 
-            Books.Add(book);
+            dbContext.Book.Add(book);
+            await dbContext.SaveChangesAsync();
 
-            return Results.CreatedAtRoute(GetBookEndpoint, new { id = book.Id }, book);
+            return Results.CreatedAtRoute(GetBookEndpoint, new { id = book.Id }, book.ToBookSummaryDto());
         });
 
         // Put /books/id
-        group.MapPut("/{id:int}", (int id, UpdateBookDto updatedBook) => {
-            int index = Books.FindIndex(book => book.Id == id);
+        group.MapPut("/{id:int}", async (int id, UpdateBookDto updatedBook, BookStoreContext dbContext) => {
+            Book? existingBook = await dbContext.Book.FindAsync(id);
 
-            if (index == 1) return Results.NotFound();
+            if (existingBook == null) return Results.NotFound("Book not found");
 
-            Books[index] = new BookSummaryDto(id, updatedBook.Title, updatedBook.Author, updatedBook.Publisher,
-                updatedBook.Isbn, updatedBook.Edition, updatedBook.Price, updatedBook.PublishDate);
+            Author? existingAuthor = await dbContext.Author.FindAsync(updatedBook.AuthorId);
+
+            if (existingAuthor == null) return Results.NotFound("Author not found");
+
+            Publisher? existingPublisher = await dbContext.Publisher.FindAsync(updatedBook.PublisherId);
+
+            if (existingPublisher == null) return Results.NotFound("Publisher not found");
+
+            dbContext.Entry(existingPublisher).CurrentValues
+                .SetValues(updatedBook.ToEntity(existingAuthor, existingPublisher, id));
+
+            await dbContext.SaveChangesAsync();
 
             return Results.NoContent();
         });
 
         // Delete /books/id
-        group.MapDelete("/{id:int}", (int id) => {
-            int index = Books.FindIndex(book => book.Id == id);
+        group.MapDelete("/{id:int}",
+            async (int id, BookStoreContext dbContext) => {
+                await dbContext.Book.Where(book => book.Id == id).ExecuteDeleteAsync();
 
-            if (index == -1) return Results.NotFound();
-
-            Books.RemoveAt(index);
-
-            _nextId--;
-
-            return Results.NoContent();
-        });
+                return Results.NoContent();
+            });
 
         return group;
     }
