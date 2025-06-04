cca6f73c3b5e23af1e4469f0b6fbd7aea6f671be
Diego Alexandre <ddsa.1988@outlook.com>
2025-06-04 20:41:22 -0300

    commit

--- BookAspnetCore/PartyInvites/Controllers/HomeController.cs
+++ BookAspnetCore/PartyInvites/Controllers/HomeController.cs
@@ -6,19 +6,11 @@ namespace PartyInvites.Controllers;
 public class HomeController : Controller {
     public ViewResult Index() {
         const string view = "Index";
-
-        GuestResponse[] responses = [
-            new("Diego", "diego@outlook.com", "984211", true),
-            new("Amanda", "amanda@outlook.com", "74585", false),
-            new("Amora", "amora@outlook.com", "123548", true),
-            new("Ameixa", "ameixa@outlook.com", "452183", true)
-        ];
-        return View(view, responses);
+        return View(view);
     }
 
     public ViewResult RsvpForm() {
         const string view = "RsvpForm";
-
         return View(view);
     }
 }
