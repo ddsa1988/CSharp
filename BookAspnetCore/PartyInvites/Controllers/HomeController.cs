713784a5e2f22ae7e9fbd3ac86be27d323609a8e
Diego Alexandre <ddsa.1988@outlook.com>
2025-06-04 22:47:05 -0300

    commit

--- BookAspnetCore/PartyInvites/Controllers/HomeController.cs
+++ BookAspnetCore/PartyInvites/Controllers/HomeController.cs
@@ -9,8 +9,16 @@ public class HomeController : Controller {
         return View(view);
     }
 
+    [HttpGet]
     public ViewResult RsvpForm() {
         const string view = "RsvpForm";
+        return View(view);
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse) {
+        const string view = "RsvpForm";
+        Console.WriteLine(guestResponse);
         return View(view);
     }
 }
