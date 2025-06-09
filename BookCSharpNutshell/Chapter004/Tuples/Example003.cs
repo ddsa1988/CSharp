1a815d2e2138ad7cb5b6e436621d63ee864c3e8f
Diego Alexandre <d.alexandre@acxautomation.com>
2025-06-09 16:27:29 -0300

    commit

--- BookCSharpNutshell/Chapter004/Tuples/Example003.cs
+++ BookCSharpNutshell/Chapter004/Tuples/Example003.cs
@@ -3,5 +3,20 @@
 public static class Example003 {
     public static void Run() {
         // Naming Tuple Elements
+        
+        (string name, int age) bob = ("Bob", 42);
+        (string name, int age) person = GetPerson();
+        
+        Console.WriteLine(bob);
+        Console.WriteLine(person);
+        
+        Console.WriteLine();
+        
+        Console.WriteLine("{0} = {1}", nameof(bob.name), bob.name);
+        Console.WriteLine("{0} = {1}", nameof(person.name), person.name);
+    }
+    
+    private static (string name, int age) GetPerson() {
+        return ("Person", 32);
     }
 }
