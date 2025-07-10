9b7ffe145bcece5a1a23fd3c3f37b33b3c6c553d
Diego Alexandre <d.alexandre@acxautomation.com>
2025-07-10 15:14:31 -0300

    commit

--- BookCSharpNutshell/Chapter006/DateAndTime/Example001.cs
+++ BookCSharpNutshell/Chapter006/DateAndTime/Example001.cs
@@ -3,5 +3,13 @@
 public static class Example001 {
     public static void Run() {
         // TimeSpan represents an interval of time—or a time of the day
+
+        var sp1 = new TimeSpan(10, 12, 25, 45, 158, 410);
+        var sp2 = new TimeSpan(1837430);
+
+        Console.WriteLine("Time span => " + sp1);
+        Console.WriteLine("Time span => " + sp2);
+        Console.WriteLine("Milliseconds => " + sp2.Milliseconds);
+        Console.WriteLine("Microseconds => " + sp2.Microseconds);
     }
 }
