27e8d180d1dc3730ceccc6b7d0d11137f853756d
Diego Alexandre <ddsa.1988@outlook.com>
2025-08-06 18:41:42 -0300

    commit

--- BookAspnetCore/SportStore/Controllers/HomeController.cs
+++ BookAspnetCore/SportStore/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
 
 namespace SportStore.Controllers;
 
 public class HomeController : Controller {
+    private readonly IStoreRepository _storeRepository;
+
+    public HomeController(IStoreRepository storeRepository) {
+        _storeRepository = storeRepository;
+    }
+
     public IActionResult Index() {
-        return View();
+        return View(_storeRepository.Products);
     }
 }
