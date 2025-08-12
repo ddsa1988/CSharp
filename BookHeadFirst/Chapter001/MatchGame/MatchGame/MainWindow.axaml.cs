6df8e7d118a6d57f4afa471543e43e1176eea5bd
Diego Alexandre <d.alexandre@acxautomation.com>
2025-08-12 13:15:38 -0300

    commit

--- BookHeadFirst/Chapter001/MatchGame/MatchGame/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter001/MatchGame/MatchGame/MainWindow.axaml.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace MatchGame;
 
 public partial class MainWindow : Window {
+    private TextBlock? _lastTextBlockClicked;
+    private bool _findingMatch;
+
     public MainWindow() {
         InitializeComponent();
         SetUpGame();
@@ -32,5 +36,30 @@ public partial class MainWindow : Window {
             textBlock.Text = nextEmoji;
             emojis.RemoveAt(index);
         }
+    }
+
+    private void TextBlock_Pressed(object? sender, PointerPressedEventArgs e) {
+        var textBlock = sender as TextBlock;
+
+        if (textBlock == null) return;
+
+        if (!_findingMatch) {
+            textBlock.IsVisible = false;
+            _lastTextBlockClicked = textBlock;
+            _findingMatch = true;
+            return;
+        }
+
+        if (_lastTextBlockClicked?.Text == textBlock.Text) {
+            textBlock.IsVisible = false;
+            _findingMatch = false;
+            return;
+        }
+
+        if (_lastTextBlockClicked != null) {
+            _lastTextBlockClicked.IsVisible = true;
+        }
+
+        _findingMatch = false;
     }
 }
