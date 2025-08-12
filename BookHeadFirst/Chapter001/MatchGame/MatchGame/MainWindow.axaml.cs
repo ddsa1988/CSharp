4792bf5cfc2ba40751052fc91431e4ee6a297c18
Diego Alexandre <d.alexandre@acxautomation.com>
2025-08-12 08:53:30 -0300

    commit

--- BookHeadFirst/Chapter001/MatchGame/MatchGame/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter001/MatchGame/MatchGame/MainWindow.axaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
 
 namespace MatchGame;
@@ -5,5 +8,29 @@ namespace MatchGame;
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+        SetUpGame();
+    }
+
+    private void SetUpGame() {
+        string[] originalEmojis = [
+            "🐶", "🐶",
+            "🐺", "🐺",
+            "🐮", "🐮",
+            "🦊", "🦊",
+            "🐱", "🐱",
+            "🦁", "🦁",
+            "🐯", "🐯",
+            "🐹", "🐹",
+        ];
+
+        var random = new Random();
+        List<string> emojis = originalEmojis.ToList();
+
+        foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>()) {
+            int index = random.Next(emojis.Count);
+            string nextEmoji = emojis[index];
+            textBlock.Text = nextEmoji;
+            emojis.RemoveAt(index);
+        }
     }
 }
