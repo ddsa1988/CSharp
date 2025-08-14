aa72f3c1c6487b2d0dff76945df5e8e3b7427b64
Diego Alexandre <d.alexandre@acxautomation.com>
2025-08-14 15:58:13 -0300

    commit

--- BookAspnetCore/Platform/Program.cs
+++ BookAspnetCore/Platform/Program.cs
@@ -1,6 +1,16 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
+// Custom middleware
+app.Use(async (context, next) => {
+    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true") {
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Custom middleware \n");
+    }
+
+    await next();
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
