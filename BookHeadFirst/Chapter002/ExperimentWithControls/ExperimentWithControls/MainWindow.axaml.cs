7a92915be0103c224d8001824da2b05fbec131ab
Diego Alexandre <ddsa.1988@outlook.com>
2025-08-16 00:12:56 -0300

    commit

--- BookHeadFirst/Chapter002/ExperimentWithControls/ExperimentWithControls/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter002/ExperimentWithControls/ExperimentWithControls/MainWindow.axaml.cs
@@ -1,9 +1,55 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 
 namespace ExperimentWithControls;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void TextBoxChanged(object? sender, TextChangedEventArgs e) {
+        if (Number == null) return;
+
+        bool isNumberValid = int.TryParse(NumberTextBox.Text, out int number);
+
+        Number.Text = isNumberValid ? number.ToString() : "0";
+    }
+
+    private void SliderValueChanged(object? sender, RangeBaseValueChangedEventArgs e) {
+        if (Number == null) return;
+
+        Number.Text = SmallSlider.Value.ToString("0");
+    }
+
+    private void RadioButtonChecked(object? sender, RoutedEventArgs e) {
+        if (Number == null) return;
+
+        var radioButton = sender as RadioButton;
+
+        if (radioButton?.Content == null) return;
+
+        Number.Text = radioButton.Content.ToString();
+    }
+
+    private void MyListBoxSelectionChanged(object? sender, SelectionChangedEventArgs e) {
+        if (Number == null) return;
+
+        var listBoxItem = MyListBox.SelectedItem as ListBoxItem;
+
+        if (listBoxItem?.Content == null) return;
+
+        Number.Text = listBoxItem.Content.ToString();
+    }
+
+    private void ReadOnlyComboBoxChanged(object? sender, SelectionChangedEventArgs e) {
+        if (Number == null) return;
+
+        var listBox = ReadOnlyComboBox.SelectedItem as ListBoxItem;
+
+        if (listBox?.Content == null) return;
+
+        Number.Text = listBox.Content.ToString();
     }
 }
--- BookHeadFirst/README.txt
+++ BookHeadFirst/README.txt
@@ -1,3 +1,3 @@
 Book Head First C# - O' Reilly, 4rd Edition
 
-Page => 75
+Page => 104
