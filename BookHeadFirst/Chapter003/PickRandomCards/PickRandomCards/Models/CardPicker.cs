8d94455cad198223ccfb89a1c1b8117d3099c204
Diego Alexandre <d.alexandre@acxautomation.com>
2025-08-18 13:28:53 -0300

    commit

--- BookHeadFirst/Chapter003/PickRandomCards/PickRandomCards/Models/CardPicker.cs
+++ BookHeadFirst/Chapter003/PickRandomCards/PickRandomCards/Models/CardPicker.cs
@@ -1,7 +1,7 @@
 namespace PickRandomCards.Models;
 
 public static class CardPicker {
-    private static Random _random = new Random();
+    private static readonly Random Random = new Random();
 
     public static IEnumerable<string> GetRandomCards(int numberOfCards) {
         string[] randomCards = new string[numberOfCards];
@@ -17,14 +17,14 @@ public static class CardPicker {
 
     private static string GetRandomValue() {
         string[] values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
-        int index = _random.Next(0, values.Length);
+        int index = Random.Next(0, values.Length);
 
         return values[index];
     }
 
     private static string GetRandomSuit() {
         string[] suits = ["Diamonds", "Spades", "Hearts", "Clubs"];
-        int index = _random.Next(suits.Length);
+        int index = Random.Next(suits.Length);
 
         return suits[index];
     }
