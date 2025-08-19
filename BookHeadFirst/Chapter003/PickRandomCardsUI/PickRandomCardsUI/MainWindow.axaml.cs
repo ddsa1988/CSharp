f384d6b1cb1b3b2f7ad73e66163029eeab9176e9
Diego Alexandre <ddsa.1988@outlook.com>
2025-08-18 23:19:04 -0300

    commit

--- BookHeadFirst/Chapter003/PickRandomCardsUI/PickRandomCardsUI/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter003/PickRandomCardsUI/PickRandomCardsUI/MainWindow.axaml.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using PickRandomCardsUI.Models;
 
 namespace PickRandomCardsUI;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void SliderValueChanged(object? sender, RangeBaseValueChangedEventArgs e) {
+        if (CardsAmount == null) return;
+
+        CardsAmount.Text = NumberOfCards.Value.ToString("0");
+    }
+
+    private void ButtonClicked(object? sender, RoutedEventArgs e) {
+        if (ListOfCards == null) return;
+
+        IEnumerable<string> pickedCars = CardPicker.GetRandomCards((int)NumberOfCards.Value);
+
+        ListOfCards.ItemsSource = pickedCars;
     }
 }
---
+++ BookHeadFirst/Chapter003/PickRandomCardsUI/PickRandomCardsUI/Models/CardPicker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace PickRandomCardsUI.Models;
+
+public static class CardPicker {
+    private static readonly Random Random = new Random();
+
+    public static IEnumerable<string> GetRandomCards(int numberOfCards) {
+        string[] randomCards = new string[numberOfCards];
+
+        for (int i = 0; i < numberOfCards; i++) {
+            string value = GetRandomValue();
+            string suit = GetRandomSuit();
+            randomCards[i] = value + " of " + suit;
+        }
+
+        return randomCards;
+    }
+
+    private static string GetRandomValue() {
+        string[] values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+        int index = Random.Next(0, values.Length);
+
+        return values[index];
+    }
+
+    private static string GetRandomSuit() {
+        string[] suits = ["Diamonds", "Spades", "Hearts", "Clubs"];
+        int index = Random.Next(suits.Length);
+
+        return suits[index];
+    }
+}
