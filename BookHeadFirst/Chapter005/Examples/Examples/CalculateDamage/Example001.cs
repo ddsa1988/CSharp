d57b3c0cfbccd31144759d04c08f07ba18f0f269
Diego Alexandre <ddsa.1988@outlook.com>
2025-08-28 23:35:13 -0300

    commit

--- BookHeadFirst/Chapter005/Examples/Examples/CalculateDamage/Example001.cs
+++ BookHeadFirst/Chapter005/Examples/Examples/CalculateDamage/Example001.cs
@@ -1,3 +1,5 @@
+using Examples.Models;
+
 namespace Examples.CalculateDamage;
 
 public static class Example001 {
@@ -6,8 +8,11 @@ public static class Example001 {
         const int diceMinValue = 1;
         const int diceMaxValue = 6;
 
-        char[] options = ['1', '2', '3'];
+        char[] options = ['0', '1', '2', '3'];
         var random = new Random();
+        var swordDamage = new SwordDamage();
+
+        int roll = 0;
 
         while (true) {
             Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
@@ -16,13 +21,15 @@ public static class Example001 {
 
             if (!options.Contains(option)) break;
 
-            int rolled = 0;
-
             for (int i = 0; i < numberDices; i++) {
-                rolled += random.Next(diceMinValue, diceMaxValue + 1);
+                roll += random.Next(diceMinValue, diceMaxValue + 1);
             }
 
-            Console.WriteLine($"{nameof(rolled)}: {rolled}");
+            swordDamage.Roll = roll;
+            swordDamage.SetMagic(option == '1' || option == '3');
+            swordDamage.SetFlaming(option == '2' || option == '3');
+
+            Console.WriteLine($"{nameof(roll)} {roll} for {swordDamage.Damage} HP");
         }
     }
 }
