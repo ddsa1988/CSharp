8b5e9ab2c4a9cade527c87e6365e0305c2044539
Diego Alexandre <d.alexandre@acxautomation.com>
2025-09-12 15:15:47 -0300

    commit

--- BookHeadFirst/Chapter007/Examples/Examples/Example007.cs
+++ BookHeadFirst/Chapter007/Examples/Examples/Example007.cs
@@ -1,5 +1,17 @@
+using Examples.Models;
+
 namespace Examples;
 
 public static class Example007 {
-    public static void Run() { }
+    public static void Run() {
+        IClown fingersTheClown = new ScaryScary("big red nose", 14);
+        fingersTheClown.Honk();
+
+        var iScaryClownReference = fingersTheClown as IScaryClown;
+        iScaryClownReference?.ScareLittleChildren();
+
+        // if (fingersTheClown is IScaryClown iScaryClownReference) {
+        //     iScaryClownReference.ScareLittleChildren();
+        // }
+    }
 }
