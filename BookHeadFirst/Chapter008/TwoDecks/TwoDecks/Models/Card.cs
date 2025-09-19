cd196fc8eb4c19b0bfc22489828857078dd11c15
Diego Alexandre <d.alexandre@acxautomation.com>
2025-09-19 10:06:49 -0300

    commit

---
+++ BookHeadFirst/Chapter008/TwoDecks/TwoDecks/Models/Card.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+
+namespace TwoDecks.Models;
+
+public enum Suits {
+    Diamonds,
+    Spades,
+    Hearts,
+    Clubs,
+}
+
+public enum Ranks {
+    Ace = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+}
+
+public class Card {
+    public Suits Suits { get; }
+    public Ranks Rank { get; }
+
+    public Card(Ranks rank, Suits suits) {
+        Rank = rank;
+        Suits = suits;
+    }
+
+    public override string ToString() {
+        string[] facedCards = [nameof(Ranks.Ace), nameof(Ranks.Jack), nameof(Ranks.Queen), nameof(Ranks.King)];
+        string rankString = facedCards.Contains(Rank.ToString()) ? Rank.ToString() : ((int)Rank).ToString();
+
+        return $"{rankString} of {Suits}";
+    }
+}
