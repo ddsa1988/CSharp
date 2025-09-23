7cf0d68b82f8e315e0d347e67c9a400bb363e6f8
Diego Alexandre <d.alexandre@acxautomation.com>
2025-09-23 16:07:40 -0300

    commit

---
+++ BookHeadFirst/Chapter009/Examples/Examples/LinqTest/Example001.cs
@@ -0,0 +1,16 @@
+﻿namespace Examples.LinqTest;
+
+public static class Example001 {
+    public static void Run() {
+        var numbers = new List<int>();
+
+        for (int i = 0; i < 20; i++) {
+            numbers.Add(i);
+        }
+
+        IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+
+        Console.WriteLine($"{nameof(numbers)} => {string.Join(", ", numbers)}");
+        Console.WriteLine($"{nameof(firstAndLastFive)} => {string.Join(", ", firstAndLastFive)}");
+    }
+}
