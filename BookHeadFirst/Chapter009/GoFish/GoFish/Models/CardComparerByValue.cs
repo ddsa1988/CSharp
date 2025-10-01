74f1dae0a63406c69fab38c0e00d0abe5c11e149
Diego Alexandre <d.alexandre@acxautomation.com>
2025-10-01 13:11:43 -0300

    commit

---
+++ BookHeadFirst/Chapter009/GoFish/GoFish/Models/CardComparerByValue.cs
@@ -0,0 +1,12 @@
+namespace GoFish.Models;
+
+public class CardComparerByValue : IComparer<Card> {
+    public int Compare(Card? x, Card? y) {
+        if (x == null || y == null) return 0;
+        if (x.Rank > y.Rank) return 1;
+        if (x.Rank < y.Rank) return -1;
+        if (x.Suit > y.Suit) return 1;
+        if (x.Suit < y.Suit) return -1;
+        return 0;
+    }
+}
