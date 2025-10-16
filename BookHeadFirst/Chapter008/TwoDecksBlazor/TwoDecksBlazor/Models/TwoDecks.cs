a32c6b3505d4016e8569d6a4ade8a1a277272b97
Diego Alexandre <d.alexandre@acxautomation.com>
2025-10-16 15:03:43 -0300

    commit

--- BookHeadFirst/Chapter008/TwoDecksBlazor/TwoDecksBlazor/Models/TwoDecks.cs
+++ BookHeadFirst/Chapter008/TwoDecksBlazor/TwoDecksBlazor/Models/TwoDecks.cs
@@ -1,38 +1,57 @@
 namespace TwoDecksBlazor.Models;
 
 public class TwoDecks {
-    public readonly Deck LeftDeck = new Deck();
-    public readonly Deck RightDeck = new Deck();
+    private readonly Deck _leftDeck = new Deck();
+    private readonly Deck _rightDeck = new Deck();
 
     public TwoDecks() {
         Clear();
     }
 
+    public int LeftDeckCount => _leftDeck.Count;
+    public int RightDeckCount => _rightDeck.Count;
+
+    public string LeftDeckCardName(int index) {
+        if (index < 0 || index >= _leftDeck.Count) {
+            return string.Empty;
+        }
+
+        return _leftDeck[index].ToString();
+    }
+
+    public string RightDeckCardName(int index) {
+        if (index < 0 || index >= _rightDeck.Count) {
+            return string.Empty;
+        }
+
+        return _rightDeck[index].ToString();
+    }
+
     public void Shuffle() {
-        LeftDeck.Shuffle();
+        _leftDeck.Shuffle();
     }
 
     public void Reset() {
-        LeftDeck.Reset();
+        _leftDeck.Reset();
     }
 
     public void Clear() {
-        RightDeck.Clear();
+        _rightDeck.Clear();
     }
 
     public void Sort() {
-        RightDeck.Sort(new CardComparer());
+        _rightDeck.Sort(new CardComparer());
     }
 
     public void MoveCard(int index, bool leftToRight) {
         if (leftToRight) {
-            Card card = LeftDeck[index];
-            LeftDeck.RemoveAt(index);
-            RightDeck.Add(card);
+            Card card = _leftDeck[index];
+            _leftDeck.RemoveAt(index);
+            _rightDeck.Add(card);
         } else {
-            Card card = RightDeck[index];
-            RightDeck.RemoveAt(index);
-            LeftDeck.Add(card);
+            Card card = _rightDeck[index];
+            _rightDeck.RemoveAt(index);
+            _leftDeck.Add(card);
         }
     }
 }
