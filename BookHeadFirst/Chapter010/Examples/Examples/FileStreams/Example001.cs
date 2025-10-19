7cb3520db5ea0e9f3b1b50886f04d7a97042e7c3
Diego Alexandre <ddsa.1988@outlook.com>
2025-10-19 09:00:29 -0300

    commit

--- BookHeadFirst/Chapter010/Examples/Examples/FileStreams/Example001.cs
+++ BookHeadFirst/Chapter010/Examples/Examples/FileStreams/Example001.cs
@@ -2,17 +2,24 @@ namespace Examples.FileStreams;
 
 public static class Example001 {
     public static void Run() {
+        const string folderName = "FirstFile.txt";
         char sepChar = Path.DirectorySeparatorChar;
         string directoryPath = $"..{sepChar}..{sepChar}..{sepChar}FileStreams{sepChar}Files";
-        string filePath = $"..{sepChar}..{sepChar}..{sepChar}FileStreams{sepChar}Files{sepChar}FirstFile.txt";
+        string filePath = $"{directoryPath}{sepChar}{folderName}";
 
-        if (!Directory.Exists(directoryPath)) {
-            return;
-        }
+        // Console.WriteLine(Environment.CurrentDirectory);
+        // Console.WriteLine(Directory.GetCurrentDirectory());
+        // Console.WriteLine(Path.GetFullPath(filePath));
+
+        // if (!Path.Exists(directoryPath)) return;
+
+        if (!Directory.Exists(directoryPath)) return;
 
         var writer = new StreamWriter(filePath, false);
         writer.WriteLine("This is a test");
         writer.Close();
+
+        if (!File.Exists(filePath)) return;
 
         var reader = new StreamReader(filePath);
 
