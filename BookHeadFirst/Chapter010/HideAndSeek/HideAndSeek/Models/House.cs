9e864d32c6699903ff8b2187d606cfe0e20c85c7
Diego Alexandre <ddsa.1988@outlook.com>
2025-10-29 22:14:33 -0300

    commit

--- BookHeadFirst/Chapter010/HideAndSeek/HideAndSeek/Models/House.cs
+++ BookHeadFirst/Chapter010/HideAndSeek/HideAndSeek/Models/House.cs
@@ -1,3 +1,4 @@
+using HideAndSeek.Enums;
 using HideAndSeek.Services;
 
 namespace HideAndSeek.Models;
@@ -7,5 +8,36 @@ public static class House {
 
     static House() {
         Entry = new Location(HouseRooms.Entry);
+        CreateLayout();
+    }
+
+    private static void CreateLayout() {
+        var garage = new Location(HouseRooms.Garage);
+        var hallway = new Location(HouseRooms.Hallway);
+        var kitchen = new Location(HouseRooms.Kitchen);
+        var bathroom = new Location(HouseRooms.Bathroom);
+        var livingRoom = new Location(HouseRooms.LivingRoom);
+        var landing = new Location(HouseRooms.Landing);
+        var masterBedroom = new Location(HouseRooms.MasterBedroom);
+        var masterBathroom = new Location(HouseRooms.MasterBathroom);
+        var secondBathroom = new Location(HouseRooms.SecondBathroom);
+        var nursery = new Location(HouseRooms.Nursery);
+        var pantry = new Location(HouseRooms.Pantry);
+        var kidsRoom = new Location(HouseRooms.KidsRoom);
+        var attic = new Location(HouseRooms.Attic);
+
+        Entry.AddExit(Direction.Out, garage);
+        Entry.AddExit(Direction.East, hallway);
+        hallway.AddExit(Direction.Northwest, kitchen);
+        hallway.AddExit(Direction.North, bathroom);
+        hallway.AddExit(Direction.South, livingRoom);
+        hallway.AddExit(Direction.Up, landing);
+        landing.AddExit(Direction.Northwest, masterBedroom);
+        masterBedroom.AddExit(Direction.East, masterBathroom);
+        landing.AddExit(Direction.West, secondBathroom);
+        landing.AddExit(Direction.Southwest, nursery);
+        landing.AddExit(Direction.South, pantry);
+        landing.AddExit(Direction.Southeast, kidsRoom);
+        landing.AddExit(Direction.Up, attic);
     }
 }
---
+++ BookHeadFirst/Chapter010/HideAndSeek/HideAndSeekTests/GameControllerTest.cs
@@ -0,0 +1,62 @@
+using HideAndSeek.Enums;
+using HideAndSeek.Models;
+using HideAndSeek.Services;
+
+namespace HideAndSeekTests;
+
+[TestClass]
+public class GameControllerTest {
+    private GameController? _gameController;
+
+    [TestInitialize]
+    public void Initialize() {
+        _gameController = new GameController();
+    }
+
+    [TestMethod]
+    public void TestMovement() {
+        if (_gameController == null) return;
+
+        Assert.AreEqual(HouseRooms.Entry, _gameController.CurrentLocation.Name);
+
+        Assert.IsFalse(_gameController.Move(Direction.Up));
+        Assert.AreEqual(HouseRooms.Entry, _gameController.CurrentLocation.Name);
+
+        Assert.IsTrue(_gameController.Move(Direction.East));
+        Assert.AreEqual(HouseRooms.Entry, _gameController.CurrentLocation.Name);
+
+        Assert.IsTrue(_gameController.Move(Direction.Up));
+        Assert.AreEqual(HouseRooms.Entry, _gameController.CurrentLocation.Name);
+
+        // Add more movement tests to the TestMovement test method
+    }
+
+    [TestMethod]
+    public void TestParseInt() {
+        if (_gameController == null) return;
+
+        string initialStatus = _gameController.Status;
+
+        Assert.AreEqual("That's not a valid direction", _gameController.ParseInput("X"));
+        Assert.AreEqual(initialStatus, _gameController.Status);
+
+        Assert.AreEqual("There's no exit in that direction", _gameController.ParseInput("Up"));
+        Assert.AreEqual(initialStatus, _gameController.Status);
+
+        Assert.AreEqual("Moving East", _gameController.ParseInput("East"));
+
+        Assert.AreEqual("You are in the Hallway. You see the following exits:" +
+                        Environment.NewLine + " - the Bathroom is to the North" +
+                        Environment.NewLine + " - the Living Room is to the South" +
+                        Environment.NewLine + " - the Entry is to the West" +
+                        Environment.NewLine + " - the Kitchen is to the Northwest" +
+                        Environment.NewLine + " - the Landing is Up", _gameController.Status);
+
+        Assert.AreEqual("Moving South", _gameController.ParseInput("South"));
+        Assert.AreEqual(
+            "You are in the Living Room. You see the following exits:" + Environment.NewLine +
+            " - the Hallway is to the North", _gameController.Status);
+
+        // Can you add more input parsing tests to the TestParseInput test method?
+    }
+}
