fc7680fad01bf7fd3ee71d9723d0f0a8e2e08a29
Diego Alexandre <d.alexandre@acxautomation.com>
2025-10-31 11:31:35 -0300

    commit

--- BasicLogics/HttpRequests/HttpRequests/CurrencyData/Example.cs
+++ BasicLogics/HttpRequests/HttpRequests/CurrencyData/Example.cs
@@ -8,7 +8,7 @@ public static class Example {
     public static async Task Run() {
         // Ex.: https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL
 
-        const int selectCurrency = 0;
+        const int selectCurrency = 3;
 
         string currencyType = selectCurrency switch {
             0 => "USD-BRL",
@@ -18,22 +18,36 @@ public static class Example {
         };
 
         using var client = new HttpClient();
-        CurrencyResponse? currency = null;
+        CurrencyResponse? content = null;
+        HttpResponseMessage? response = null;
 
         try {
-            currency =
-                await client.GetFromJsonAsync<CurrencyResponse>(
-                    $"https://economia.awesomeapi.com.br/json/last/{currencyType}");
+            response = await client.GetAsync($"https://economia.awesomeapi.com.br/json/last/{currencyType}");
         } catch (HttpRequestException e) {
-            Console.WriteLine($"HttpRequestException: {e.Message}");
+            Console.WriteLine($"Http Request Exception: {e.Message}");
         } catch (Exception e) {
-            Console.WriteLine($"GeneralException: {e.Message}");
+            Console.WriteLine($"General Exception: {e.Message}");
         }
 
-        if (currency?.CurrencyInfo == null) return;
+        if (response == null) return;
 
-        foreach ((string key, JsonElement value) in currency.CurrencyInfo) {
-            if (!currency.CurrencyInfo.TryGetValue(key, out JsonElement jsonCurrencyInfo)) continue;
+        if (!response.IsSuccessStatusCode) {
+            Console.WriteLine("Error code: {0}", response.StatusCode);
+            return;
+        }
+
+        try {
+            content = await response.Content.ReadFromJsonAsync<CurrencyResponse>();
+        } catch (OperationCanceledException e) {
+            Console.WriteLine($"Operation Canceled Exception: {e.Message}");
+        } catch (Exception e) {
+            Console.WriteLine($"General Exception: {e.Message}");
+        }
+
+        if (content?.CurrencyInfo == null) return;
+
+        foreach ((string key, JsonElement value) in content.CurrencyInfo) {
+            if (!content.CurrencyInfo.TryGetValue(key, out JsonElement jsonCurrencyInfo)) continue;
 
             var currencyInfo = JsonSerializer.Deserialize<CurrencyInfo>(jsonCurrencyInfo.ToString());
 
