bf65c50662c5f47b7ae7f4d37a592cf91ece0abd
Diego Alexandre <d.alexandre@acxautomation.com>
2025-11-07 08:27:55 -0300

    commit

---
+++ BookHeadFirst/Chapter012/Examples/Examples/HandlingExceptions/Example001.cs
@@ -0,0 +1,24 @@
+﻿namespace Examples.HandlingExceptions;
+
+public static class Example001 {
+    public static void Run() {
+        int? number = null;
+
+        while (!number.HasValue) {
+            Console.Write("Type a number: ");
+            string input = Console.ReadLine() ?? string.Empty;
+
+            try {
+                number = int.Parse(input);
+            } catch (NullReferenceException ex) {
+                Console.WriteLine("Invalid number: " + ex.Message);
+            } catch (FormatException ex) {
+                Console.WriteLine("Invalid number: " + ex.Message);
+            } catch (OverflowException ex) {
+                Console.WriteLine("Invalid number: " + ex.Message);
+            }
+        }
+
+        Console.Write("You've typed the number: " + number.Value);
+    }
+}
