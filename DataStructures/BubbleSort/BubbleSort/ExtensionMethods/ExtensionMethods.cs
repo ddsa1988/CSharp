46f31fd61c6c622236208489eda8ed92721c3cc3
Diego Alexandre <d.alexandre@acxautomation.com>
2025-11-11 07:17:20 -0300

    commit

--- DataStructures/BubbleSort/BubbleSort/ExtensionMethods/ExtensionMethods.cs
+++ DataStructures/BubbleSort/BubbleSort/ExtensionMethods/ExtensionMethods.cs
@@ -1,5 +1,22 @@
 namespace BubbleSort.ExtensionMethods;
 
 public static class ExtensionMethods {
-    public static void BubbleSort<T>(this T[] array) where T : IComparable<T> { }
+    // Bubbe sort: O(n²) => O(n x n)
+    public static void BubbleSort<T>(this T[] array) where T : IComparable<T> {
+        if (array.Length == 0) return;
+
+        int lenght = array.Length;
+        bool swapped = false;
+
+        for (int i = 0; i < lenght - 1; i++) {
+            for (int j = 0; j < lenght - i - 1; j++) {
+                if (array[j].CompareTo(array[j + 1]) <= 0) continue;
+
+                (array[j + 1], array[j]) = (array[j], array[j + 1]);
+                swapped = true;
+            }
+
+            if (!swapped) break;
+        }
+    }
 }
