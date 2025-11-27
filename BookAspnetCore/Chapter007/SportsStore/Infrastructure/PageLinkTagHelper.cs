a632a746d43502423c9a273546474e83724f956e
Diego Alexandre <d.alexandre@acxautomation.com>
2025-11-27 07:51:35 -0300

    commit

---
+++ BookAspnetCore/Chapter007/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SportsStore.Infrastructure;
+
+public class PageLinkTagHelper : TagHelper {
+    public override void Process(TagHelperContext context, TagHelperOutput output) { }
+}
