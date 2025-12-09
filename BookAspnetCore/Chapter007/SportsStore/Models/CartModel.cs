897b85954c05ed4361b853009908f9a21f8144e5
Diego Alexandre <ddsa.1988@outlook.com>
2025-12-08 23:10:12 -0300

    commit

--- BookAspnetCore/Chapter007/SportsStore/Models/CartModel.cs
+++ BookAspnetCore/Chapter007/SportsStore/Models/CartModel.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SportsStore.Infrastructure;
-using SportsStore.Models;
 
-namespace SportsStore.Pages;
+namespace SportsStore.Models;
 
 public class CartModel : PageModel {
     private IStoreRepository _repository;
-    public Models.Cart? Cart { get; set; }
+    public Cart? Cart { get; set; }
     public string ReturnUrl { get; set; } = "/";
 
     public CartModel(IStoreRepository repository) {
@@ -16,14 +15,14 @@ public class CartModel : PageModel {
 
     public void OnGet(string? returnUrl) {
         ReturnUrl = returnUrl ?? "/";
-        Cart = HttpContext.Session.GetJson<Models.Cart>("cart") ?? new Models.Cart();
+        Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
     }
 
     public IActionResult OnPost(long productId, string returnUrl) {
         Product? product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
 
         if (product == null) return RedirectToPage(new { returnUrl });
-        Cart = HttpContext.Session.GetJson<Models.Cart>("cart") ?? new Models.Cart();
+        Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         Cart.AddItem(product, 1);
         HttpContext.Session.SetJson("cart", Cart);
 
--- BookAspnetCore/Chapter007/SportsStoreTests/CartPageTests.cs
+++ BookAspnetCore/Chapter007/SportsStoreTests/CartPageTests.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Models;
-using SportsStore.Pages;
-using Cart = SportsStore.Models.Cart;
 
 namespace SportsStoreTests;
 
@@ -52,5 +50,37 @@ public class CartPageTests {
     }
 
     [Fact]
-    public void CanUpdateCart() { }
+    public void CanUpdateCart() {
+        // Arrange
+        var mockRepository = new Mock<IStoreRepository>();
+        mockRepository.Setup(repository => repository.Products)
+            .Returns((new[] { new Product { ProductId = 1, Name = "P1" } }).AsQueryable());
+
+        var testCart = new Cart();
+
+        var mockSession = new Mock<ISession>();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Callback<string, byte[]>((_, val) => {
+                testCart = JsonSerializer.Deserialize<Cart>(Encoding.UTF8.GetString(val));
+            });
+
+        var mockContext = new Mock<HttpContext>();
+        mockContext.Setup(context => context.Session).Returns(mockSession.Object);
+
+        // Act
+        var cartModel = new CartModel(mockRepository.Object) {
+            PageContext = new PageContext(new ActionContext {
+                HttpContext = mockContext.Object,
+                RouteData = new RouteData(),
+                ActionDescriptor = new PageActionDescriptor(),
+            })
+        };
+
+        cartModel.OnPost(1, "myUrl");
+
+        // Assert
+        Assert.Single(testCart.CartLines);
+        Assert.Equal("P1", testCart.CartLines.First().Product.Name);
+        Assert.Equal(1, testCart.CartLines.First().Quantity);
+    }
 }
