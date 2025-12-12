aeea478a44138de070def291f6d666d1d8b41752
Diego Alexandre <d.alexandre@acxautomation.com>
2025-12-12 13:32:21 -0300

    commit

--- BookAspnetCore/Chapter007/SportsStore/Controllers/OrderController.cs
+++ BookAspnetCore/Chapter007/SportsStore/Controllers/OrderController.cs
@@ -4,7 +4,31 @@ using SportsStore.Models;
 namespace SportsStore.Controllers;
 
 public class OrderController : Controller {
+    private readonly IOrderRepository _orderRepository;
+    private readonly Cart _cart;
+
+    public OrderController(IOrderRepository orderRepository, Cart cart) {
+        _orderRepository = orderRepository;
+        _cart = cart;
+    }
+
+    [HttpGet]
     public ViewResult Checkout() {
         return View(new Order());
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Order order) {
+        if (_cart.CartLines.Count == 0) {
+            ModelState.AddModelError("", "Sorry, your cart is empty!");
+        }
+
+        if (!ModelState.IsValid) return View();
+
+        order.CartLines = _cart.CartLines.ToArray();
+        _orderRepository.SaverOrder(order);
+        _cart.Clear();
+
+        return RedirectToPage("/Completed", new { order.OrderId });
     }
 }
