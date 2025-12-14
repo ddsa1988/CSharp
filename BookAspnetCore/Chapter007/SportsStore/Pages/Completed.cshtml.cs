1e69c993eeacefe9287c709f061958e4446f36f1
Diego Alexandre <ddsa.1988@outlook.com>
2025-12-14 00:02:23 -0300

    commit

---
+++ BookAspnetCore/Chapter007/SportsStore/Pages/Completed.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportsStore.Pages;
+
+public class Completed : PageModel {
+    public void OnGet() {
+        
+    }
+}
--- BookAspnetCore/Chapter007/SportsStoreTests/OrderControllerTests.cs
+++ BookAspnetCore/Chapter007/SportsStoreTests/OrderControllerTests.cs
@@ -32,18 +32,48 @@ public class OrderControllerTests {
     [Fact]
     public void CannotCheckoutInvalidShippingDetails() {
         // Arrange
+        var mock = new Mock<IOrderRepository>();
+
+        var cart = new Cart();
+        cart.AddItem(new Product(), 1);
+
+        var controller = new OrderController(mock.Object, cart);
+        controller.ModelState.AddModelError("error", "error");
 
         // Act
+        var result = controller.Checkout(new Order()) as ViewResult;
 
         // Assert
+
+        // Check that the order hasn't been passed stored
+        mock.Verify(orderRepository => orderRepository.SaverOrder(It.IsAny<Order>()), Times.Never);
+
+        // Check that the method us returning the default view
+        Assert.True(string.IsNullOrEmpty(result?.ViewName));
+
+        // Check that I am passing an invalid mode to the view
+        Assert.False(result?.ViewData.ModelState.IsValid);
     }
 
     [Fact]
     public void CanCheckoutAndSubmitOrder() {
         // Arrange
+        var mock = new Mock<IOrderRepository>();
+
+        var cart = new Cart();
+        cart.AddItem(new Product(), 1);
+
+        var controller = new OrderController(mock.Object, cart);
 
         // Act
+        var result = controller.Checkout(new Order()) as RedirectToPageResult;
 
         // Assert
+
+        // Check that the order has been stored
+        mock.Verify(orderRepository => orderRepository.SaverOrder(It.IsAny<Order>()), Times.Once);
+
+        // Check the method is redirecting to the completed action
+        Assert.Equal("/Completed", result?.PageName);
     }
 }
