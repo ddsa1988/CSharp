2761e079d0c2ad5aa546b71a3d933ff32626fc74
Diego Alexandre <ddsa.1988@outlook.com>
2025-12-14 23:00:31 -0300

    commit

--- BookAspnetCore/Chapter007/SportsStore/Models/Order.cs
+++ BookAspnetCore/Chapter007/SportsStore/Models/Order.cs
@@ -8,24 +8,31 @@ public class Order {
     [BindNever] public ICollection<CartLine> CartLines { get; set; } = new List<CartLine>();
 
     [Required(ErrorMessage = "Please enter a name")]
+    [MaxLength(30)]
     public string? Name { get; set; }
 
     [Required(ErrorMessage = "Please enter the first address line")]
+    [MaxLength(20)]
     public string? AddressLine1 { get; set; }
 
-    public string? AddressLine2 { get; set; }
-    public string? AddressLine3 { get; set; }
+    [MaxLength(20)] public string? AddressLine2 { get; set; }
+    [MaxLength(20)] public string? AddressLine3 { get; set; }
 
     [Required(ErrorMessage = "Please enter a city name")]
+    [MaxLength(20)]
     public string? City { get; set; }
 
     [Required(ErrorMessage = "Please enter a state name")]
+    [MaxLength(20)]
     public string? State { get; set; }
 
     [Required(ErrorMessage = "Please enter a country name")]
+    [MaxLength(20)]
     public string? Country { get; set; }
 
-    public string? ZipCode { get; set; }
+    [MaxLength(10)] public string? ZipCode { get; set; }
 
     public bool GiftWrap { get; set; }
+
+    [BindNever] public bool Shipped { get; set; }
 }
