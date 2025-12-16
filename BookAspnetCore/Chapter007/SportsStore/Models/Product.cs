bf3445bb7babf141cf33958f49af3796f09c59e5
Diego Alexandre <ddsa.1988@outlook.com>
2025-12-15 22:17:38 -0300

    commit

--- BookAspnetCore/Chapter007/SportsStore/Models/Product.cs
+++ BookAspnetCore/Chapter007/SportsStore/Models/Product.cs
@@ -6,20 +6,20 @@ namespace SportsStore.Models;
 public class Product {
     public long? ProductId { get; init; }
 
-    [Required(ErrorMessage = "Please enter aproduct name")]
+    [Required(ErrorMessage = "Please enter a product name")]
     [MaxLength(30)]
-    public string Name { get; init; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Please enter a description")]
     [MaxLength(50)]
-    public string Description { get; init; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
     [Required]
     [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
     [Column(TypeName = "decimal(8,2)")]
-    public decimal Price { get; init; }
+    public decimal Price { get; set; }
 
     [Required(ErrorMessage = "Please specify a category")]
     [MaxLength(30)]
-    public string Category { get; init; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
 }
