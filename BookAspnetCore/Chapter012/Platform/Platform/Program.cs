e7b653d26d72f1f79559951996357c0d6f3c6b7d
Diego Alexandre <ddsa.1988@outlook.com>
2025-12-23 23:35:22 -0300

    commit

--- BookAspnetCore/Chapter012/Platform/Platform/Program.cs
+++ BookAspnetCore/Chapter012/Platform/Platform/Program.cs
@@ -2,6 +2,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
 app.Use(async (context, next) => {
+    await next();
+    await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) => {
     if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true") {
         context.Response.ContentType = "text/plain";
         await context.Response.WriteAsync("Custom Middleware\n");
