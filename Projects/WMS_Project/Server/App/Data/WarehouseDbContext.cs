b82e7ab73aba2f008c8ad1bd3d9d93546eb111c4
Diego Alexandre <ddsa.1988@outlook.com>
2026-02-11 23:20:54 -0300

    commit

---
+++ Projects/WMS_Project/Server/App/Data/WarehouseDbContext.cs
@@ -0,0 +1,14 @@
+using App.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Data;
+
+public class WarehouseDbContext : DbContext {
+    public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Manufacturer> Manufacturers { get; set; }
+    public DbSet<Component> Components { get; set; }
+    public DbSet<Project> Projects { get; set; }
+}
--- Projects/WMS_Project/Server/App/Program.cs
+++ Projects/WMS_Project/Server/App/Program.cs
@@ -1,6 +1,13 @@
+using App.Data;
 using App.Endpoints;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string? warehouseConnection = builder.Configuration.GetConnectionString("WarehouseConnection");
+
+builder.Services.AddSqlite<WarehouseDbContext>(warehouseConnection);
+
+//builder.Services.AddDbContext<WarehouseDbContext>(options => { options.UseSqlite(warehouseConnection); });
 
 WebApplication app = builder.Build();
 
