10276c74c99b0b594f599c62c34c041ee683814d
Diego Alexandre <ddsa.1988@outlook.com>
2026-03-01 19:47:42 -0300

    commit

--- Projects/WMS_Project/Server/App/Endpoints/ManufacturerEndpoints.cs
+++ Projects/WMS_Project/Server/App/Endpoints/ManufacturerEndpoints.cs
@@ -1,3 +1,37 @@
+using App.Data;
+using App.Entities;
+using App.Mapping;
+using Microsoft.EntityFrameworkCore;
+
 namespace App.Endpoints;
 
-public static class ManufacturerEndpoints { }
+public static class ManufacturerEndpoints {
+    public static RouteGroupBuilder MapManufacturerEndpoints(this WebApplication app) {
+        const string getManufacturerEndpointName = "GetManufacturer";
+
+        RouteGroupBuilder group = app.MapGroup("manufacturers").WithParameterValidation();
+
+        // GET
+        group.MapGet("/", async (WarehouseDbContext dbContext) =>
+            await dbContext.Manufacturers
+                .Select(manufacturer => manufacturer.ToDto())
+                .AsNoTracking()
+                .ToListAsync());
+
+        group.MapGet("/{id:long}", async (long id, WarehouseDbContext dbContext) => {
+            Manufacturer? manufacturer = await dbContext.Manufacturers.FindAsync(id);
+
+            return manufacturer == null ? Results.NotFound() : Results.Ok(manufacturer.ToDto());
+        }).WithName(getManufacturerEndpointName);
+        
+        // POST
+
+
+        // PUT
+
+
+        // DELETE
+
+        return group;
+    }
+}
---
+++ Projects/WMS_Project/Server/App/Mapping/ManufacturerMapping.cs
@@ -0,0 +1,23 @@
+using App.Dto.Manufacturer;
+using App.Entities;
+
+namespace App.Mapping;
+
+public static class ManufacturerMapping {
+    public static Manufacturer ToEntity(this UpdateManufacturerDto manufacturerDto, long id) {
+        Manufacturer manufacturer = new() {
+            Id = id,
+            Name = manufacturerDto.Name,
+            Description = manufacturerDto.Description,
+            IsDeleted = false
+        };
+
+        return manufacturer;
+    }
+
+    public static ManufacturerDto ToDto(this Manufacturer manufacturer) {
+        ManufacturerDto manufacturerDto = new(manufacturer.Id, manufacturer.Name, manufacturer.Description);
+
+        return manufacturerDto;
+    }
+}
