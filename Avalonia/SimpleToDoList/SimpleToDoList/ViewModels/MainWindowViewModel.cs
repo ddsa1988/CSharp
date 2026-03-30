ca19ee29ca9297f93598e63281b87025489c46db
Diego Alexandre <d.alexandre@ACX-AD.local>
2026-03-30 14:58:18 -0300

    commit

--- Avalonia/SimpleToDoList/SimpleToDoList/ViewModels/MainWindowViewModel.cs
+++ Avalonia/SimpleToDoList/SimpleToDoList/ViewModels/MainWindowViewModel.cs
@@ -5,11 +5,13 @@ using CommunityToolkit.Mvvm.Input;
 namespace SimpleToDoList.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase {
+    // This attribute will invalidate the command each time this property changes
     [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(AddItemCommand))]
     private string? _newItemContent;
 
     private ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = [];
 
+    // Returns if a new Item can be added. We require to have the NewItem some Text
     private bool CanAddItem() => !string.IsNullOrWhiteSpace(NewItemContent);
 
     [RelayCommand(CanExecute = nameof(CanAddItem))]
