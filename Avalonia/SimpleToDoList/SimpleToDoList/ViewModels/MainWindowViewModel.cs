49c9eeb306679196b5a0a81736997b44103b403f
Diego Alexandre <d.alexandre@ACX-AD.local>
2026-03-30 07:46:52 -0300

    commit

--- Avalonia/SimpleToDoList/SimpleToDoList/ViewModels/MainWindowViewModel.cs
+++ Avalonia/SimpleToDoList/SimpleToDoList/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,26 @@
-﻿namespace SimpleToDoList.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SimpleToDoList.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(AddItemCommand))]
+    private string? _newItemContent;
+
+    private ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = [];
+
+    private bool CanAddItem() => !string.IsNullOrWhiteSpace(NewItemContent);
+
+    [RelayCommand(CanExecute = nameof(CanAddItem))]
+    private void AddItem() {
+        ToDoItems.Add(new ToDoItemViewModel() { Content = NewItemContent });
+
+        NewItemContent = null;
+    }
+
+    [RelayCommand]
+    private void RemoveItem(ToDoItemViewModel item) {
+        ToDoItems.Remove(item);
+    }
 }
