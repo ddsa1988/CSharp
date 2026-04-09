afa97cdecc90cf34a05ea30eeb67e9e3c872a9f4
Diego Alexandre <d.alexandre@ACX-AD.local>
2026-04-09 07:20:52 -0300

    commit

---
+++ Avalonia/SimpleToDoList/SimpleToDoList/Services/ToDoListFileService.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using SimpleToDoList.Models;
+
+namespace SimpleToDoList.Services;
+
+public static class ToDoListFileService {
+    private const string FolderName = "Database";
+    private const string FileName = "ToDoList.json";
+    private static readonly string DirectoryPath = Path.Combine(AppContext.BaseDirectory, FolderName);
+    private static readonly string FilePath = Path.Combine(DirectoryPath,  FileName);
+
+    public static async Task SaveToFileAsync(IEnumerable<ToDoItem> itemsToSave) {
+        IEnumerable<ToDoItem> items = itemsToSave.ToArray();
+        
+        if (items.Any()) return;
+
+        if (!Directory.Exists(DirectoryPath)) {
+            Directory.CreateDirectory(DirectoryPath);
+        }
+
+        if (File.Exists(FilePath)) {
+            File.Delete(FilePath);
+        }
+
+        try {
+            string data = JsonSerializer.Serialize(items);
+            await File.WriteAllTextAsync(FilePath, data);
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
