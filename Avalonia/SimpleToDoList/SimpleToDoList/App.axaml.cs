c6f4b3828518225b31874c4acf4d2e8c16669294
Diego Alexandre <d.alexandre@ACX-AD.local>
2026-04-09 07:54:03 -0300

    commit

--- Avalonia/SimpleToDoList/SimpleToDoList/App.axaml.cs
+++ Avalonia/SimpleToDoList/SimpleToDoList/App.axaml.cs
@@ -1,15 +1,21 @@
+using System;
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
+using SimpleToDoList.Models;
+using SimpleToDoList.Services;
 using SimpleToDoList.ViewModels;
 using SimpleToDoList.Views;
 
 namespace SimpleToDoList;
 
 public partial class App : Application {
+    private readonly MainWindowViewModel _mainWindowViewModel = new();
+    private bool _canClose;
+    
     public override void Initialize() {
         AvaloniaXamlLoader.Load(this);
     }
@@ -20,21 +26,45 @@ public partial class App : Application {
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow {
-                DataContext = new MainWindowViewModel(),
+                DataContext = _mainWindowViewModel
             };
+
+            // Listen to the ShutdownRequested-event
+            desktop.ShutdownRequested += DesktopOnShutdownRequested;
         }
 
         base.OnFrameworkInitializationCompleted();
     }
 
-    private void DisableAvaloniaDataAnnotationValidation() {
+    private static void DisableAvaloniaDataAnnotationValidation() {
         // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
+        DataAnnotationsValidationPlugin[] dataValidationPluginsToRemove =
             BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
 
         // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove) {
+        foreach (DataAnnotationsValidationPlugin plugin in dataValidationPluginsToRemove) {
             BindingPlugins.DataValidators.Remove(plugin);
+        }
+    }
+
+    private async void DesktopOnShutdownRequested(object? sender, ShutdownRequestedEventArgs args) {
+        try {
+            args.Cancel = !_canClose;
+
+            if (_canClose) return;
+
+            IEnumerable<ToDoItem> itemsToSave = _mainWindowViewModel.ToDoItems.Select(item => item.GetToDoItem());
+
+            await ToDoListFileService.SaveToFileAsync(itemsToSave);
+        
+            _canClose = true;
+
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                desktop.Shutdown();
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
         }
     }
 }
--- Avalonia/SimpleToDoList/SimpleToDoList/Services/ToDoListFileService.cs
+++ Avalonia/SimpleToDoList/SimpleToDoList/Services/ToDoListFileService.cs
@@ -12,12 +12,20 @@ public static class ToDoListFileService {
     private const string FolderName = "Database";
     private const string FileName = "ToDoList.json";
     private static readonly string DirectoryPath = Path.Combine(AppContext.BaseDirectory, FolderName);
-    private static readonly string FilePath = Path.Combine(DirectoryPath,  FileName);
+    private static readonly string FilePath = Path.Combine(DirectoryPath, FileName);
+
+    private static readonly JsonSerializerOptions JsonWriteOptions = new() {
+        WriteIndented = true
+    };
+
+    private static readonly JsonSerializerOptions JsonReadOptions = new() {
+        AllowTrailingCommas = true
+    };
 
     public static async Task SaveToFileAsync(IEnumerable<ToDoItem> itemsToSave) {
         IEnumerable<ToDoItem> items = itemsToSave.ToArray();
-        
-        if (items.Any()) return;
+
+        if (!items.Any()) return;
 
         if (!Directory.Exists(DirectoryPath)) {
             Directory.CreateDirectory(DirectoryPath);
@@ -28,7 +36,7 @@ public static class ToDoListFileService {
         }
 
         try {
-            string data = JsonSerializer.Serialize(items);
+            string data = JsonSerializer.Serialize(items, JsonWriteOptions);
             await File.WriteAllTextAsync(FilePath, data);
         }
         catch (Exception ex) {
