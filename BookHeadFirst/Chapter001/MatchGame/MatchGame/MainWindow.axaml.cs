f296b24dcb9a64d3237ff0d9312e97ca6a6b4e61
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-13 20:05:37 -0300

    commit

--- BookHeadFirst/Chapter001/MatchGame/MatchGame/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter001/MatchGame/MatchGame/MainWindow.axaml.cs
@@ -5,18 +5,20 @@ using MatchGame.Models;
 namespace MatchGame;
 
 public partial class MainWindow : Window {
-    private readonly Game Game;
+    private readonly Game _game;
 
     public MainWindow() {
         InitializeComponent();
 
-        Game = new Game(MainGrid);
-        Game.SetUpGame();
+        _game = new Game(MainGrid);
+        _game.SetUpGame();
     }
 
     private void TextBlockClicked(object? sender, PointerPressedEventArgs e) {
         var textBlock = sender as TextBlock;
 
         if (textBlock == null) return;
+
+        _game.Test(textBlock);
     }
 }
--- BookHeadFirst/Chapter001/MatchGame/MatchGame/Models/Game.cs
+++ BookHeadFirst/Chapter001/MatchGame/MatchGame/Models/Game.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 using Avalonia.Controls;
 
 namespace MatchGame.Models;
 
 public partial class Game {
+    private readonly Random _random = new();
     private readonly Grid _gridLayout;
+    private readonly int _gridRows;
+    private readonly int _gridColumns;
     private readonly string[,] _gridEmojis;
 
     private readonly string[] _sourceEmojis =
@@ -17,32 +21,33 @@ public partial class Game {
 
     public Game(Grid gridLayout) {
         _gridLayout = gridLayout;
-        _gridEmojis = new string[_gridLayout.RowDefinitions.Count, _gridLayout.ColumnDefinitions.Count];
+        _gridRows = _gridLayout.RowDefinitions.Count - 1;
+        _gridColumns = _gridLayout.ColumnDefinitions.Count;
+        _gridEmojis = new string[_gridRows, _gridColumns];
     }
 
     [GeneratedRegex(@"\d{2}$")]
     private static partial Regex FindLastTwoDigitsRegex();
 
-    public void SetUpGame() {
-        var random = new Random();
-        int maxNumberEmojis = _gridLayout.RowDefinitions.Count * _gridLayout.ColumnDefinitions.Count;
+    private void GenerateSelectedEmojis() {
+        int maxNumberEmojis = _gridRows * _gridColumns;
 
         while (_selectedEmojis.Count < maxNumberEmojis) {
-            int randomIndex = random.Next(0, _sourceEmojis.Length);
+            int randomIndex = _random.Next(0, _sourceEmojis.Length);
             string emoji = _sourceEmojis[randomIndex];
 
             if (_selectedEmojis.Contains(emoji)) continue;
 
             _selectedEmojis.AddRange(emoji, emoji);
         }
+    }
 
-        foreach (TextBlock textBlock in _gridLayout.Children.OfType<TextBlock>()) {
-            textBlock.Text = "?";
-        }
+    private void GenerateGridEmojis() {
+        for (int row = 0; row < _gridRows; row++) {
+            for (int col = 0; col < _gridColumns; col++) {
+                if (_selectedEmojis.Count == 0) return;
 
-        for (int row = 0; row < _gridLayout.RowDefinitions.Count; row++) {
-            for (int col = 0; col < _gridLayout.ColumnDefinitions.Count; col++) {
-                int randomIndex = random.Next(0, _selectedEmojis.Count);
+                int randomIndex = _random.Next(0, _selectedEmojis.Count);
                 string emoji = _selectedEmojis[randomIndex];
 
                 _selectedEmojis.RemoveAt(randomIndex);
@@ -51,9 +56,29 @@ public partial class Game {
         }
     }
 
+    private void GenerateGridTextBlocksText() {
+        foreach (TextBlock textBlock in _gridLayout.Children.OfType<TextBlock>()) {
+            textBlock.Text = "?";
+        }
+    }
+
+    public void SetUpGame() {
+        GenerateGridTextBlocksText();
+        GenerateSelectedEmojis();
+        GenerateGridEmojis();
+    }
+
     public void Test(TextBlock textBlock) {
         Match match = FindLastTwoDigitsRegex().Match(textBlock.Name ?? "");
 
-        Console.WriteLine(match.Value);
+        if (!match.Success) return;
+
+        bool isRowValid = int.TryParse(match.ValueSpan[0].ToString(), out int row);
+        bool isColumnValid = int.TryParse(match.ValueSpan[1].ToString(), out int column);
+
+        if (!(isRowValid && row < _gridRows)) return;
+        if (!(isColumnValid && column < _gridColumns)) return;
+
+        textBlock.Text = _gridEmojis[row, column];
     }
 }
