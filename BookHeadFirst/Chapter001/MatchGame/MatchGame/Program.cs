896503132c5d0a84a1e1028d39ba6f83588e8708
Diego Alexandre <ddsa1988@outlook.com>
2026-05-15 11:44:39 -0300

    commit

--- BookHeadFirst/Chapter001/MatchGame/MatchGame/Program.cs
+++ BookHeadFirst/Chapter001/MatchGame/MatchGame/Program.cs
@@ -8,16 +8,16 @@ public static class Program {
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) {
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-    }
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    private static AppBuilder BuildAvaloniaApp() {
-        return AppBuilder.Configure<App>()
+    private static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
             .UsePlatformDetect()
+#if DEBUG
             .WithDeveloperTools()
+#endif
             .WithInterFont()
             .LogToTrace();
-    }
 }
