c8ea1fef724707747aa67889bc4ba97aab51b5db
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-17 17:21:38 -0300

    commit

--- BookHeadFirst/Chapter002/ExperimentWithControls/ExperimentWithControls/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter002/ExperimentWithControls/ExperimentWithControls/MainWindow.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using Avalonia.Controls;
 
 namespace ExperimentWithControls;
@@ -5,5 +7,17 @@ namespace ExperimentWithControls;
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void NumberTextBox_OnTextChanged(object? sender, TextChangedEventArgs e) {
+        var textBox = sender as TextBox;
+
+        if (textBox is null) return;
+
+        bool isNumeric = double.TryParse(textBox.Text, out double number);
+
+        if (!isNumeric) return;
+
+        Number.Text = $"{number}";
     }
 }
