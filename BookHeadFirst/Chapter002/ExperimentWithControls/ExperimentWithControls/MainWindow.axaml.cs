87168a85782c9d1d718283be1f7d98690e4bf858
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-17 23:04:29 -0300

    commit

--- BookHeadFirst/Chapter002/ExperimentWithControls/ExperimentWithControls/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter002/ExperimentWithControls/ExperimentWithControls/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Globalization;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 
 namespace ExperimentWithControls;
 
@@ -12,12 +13,63 @@ public partial class MainWindow : Window {
     private void NumberTextBox_OnTextChanged(object? sender, TextChangedEventArgs e) {
         var textBox = sender as TextBox;
 
-        if (textBox is null) return;
-
-        bool isNumeric = double.TryParse(textBox.Text, out double number);
+        bool isNumeric = double.TryParse(textBox?.Text, out double number);
 
         if (!isNumeric) return;
 
-        Number.Text = $"{number}";
+        Number?.Text = $"{number}";
+    }
+
+    private void RadioButton_OnClick(object? sender, RoutedEventArgs e) {
+        var radioButton = sender as RadioButton;
+
+        bool isNumeric = double.TryParse(radioButton?.Content?.ToString(), out double number);
+
+        if (!isNumeric) return;
+
+        Number?.Text = $"{number}";
+    }
+
+    private void ListBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e) {
+        var listBox = sender as ListBox;
+        var listBoxItem = listBox?.SelectedItem as ListBoxItem;
+
+        bool isNumeric = double.TryParse(listBoxItem?.Content?.ToString(), out double number);
+
+        if (!isNumeric) return;
+
+        Number?.Text = $"{number}";
+    }
+
+    private void ComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e) {
+        var comboBox = sender as ComboBox;
+        var comboBoxItem = comboBox?.SelectedItem as ComboBoxItem;
+
+        bool isNumeric = double.TryParse(comboBoxItem?.Content?.ToString(), out double number);
+
+        if (!isNumeric) return;
+
+        Number?.Text = $"{number}";
+    }
+
+    private void ComboBoxEditable_OnSelectionChanged(object? sender, SelectionChangedEventArgs e) {
+        var comboBox = sender as ComboBox;
+        var comboBoxItem = comboBox?.SelectedItem as ComboBoxItem;
+
+        bool isNumeric = double.TryParse(comboBoxItem?.Content?.ToString(), out double number);
+
+        if (!isNumeric) return;
+
+        Number?.Text = $"{number}";
+    }
+
+    private void Slider_OnValueChanged(object? sender, RangeBaseValueChangedEventArgs e) {
+        var slider = sender as Slider;
+
+        bool isNumeric = double.TryParse(slider?.Value.ToString("0"), out double number);
+
+        if (!isNumeric) return;
+
+        Number?.Text = $"{number}";
     }
 }
