d9336f05333910f7571eedd052150940cd9c8e2f
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-18 23:13:44 -0300

    commit

--- BookHeadFirst/Chapter003/PickCardUI/PickCardUI/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter003/PickCardUI/PickCardUI/MainWindow.axaml.cs
@@ -1,9 +1,15 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 
 namespace PickCardUI;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void Button_OnPointerPressed(object? sender, PointerPressedEventArgs e) {
+        throw new System.NotImplementedException();
     }
 }
