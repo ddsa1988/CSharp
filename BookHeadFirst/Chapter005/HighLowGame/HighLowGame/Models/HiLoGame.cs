51e40d59a8c5bc7cf115ac7178ebb1cb46c57c3c
Diego Alexandre <ddsa1988@outlook.com>
2026-05-25 14:42:03 -0300

    commit

--- BookHeadFirst/Chapter005/HighLowGame/HighLowGame/Models/HiLoGame.cs
+++ BookHeadFirst/Chapter005/HighLowGame/HighLowGame/Models/HiLoGame.cs
@@ -5,20 +5,40 @@ public static class HiLoGame {
     public const int Minimum = 1;
     public const int Maximum = 10;
     private static int _pot;
-    public static int CurrentNumber { get; private set; }
+    private static int _currentNumber;
 
     static HiLoGame() {
-        CurrentNumber = Random.Next(Minimum, Maximum + 1);
+        _currentNumber = Random.Next(Minimum, Maximum + 1);
         _pot = 10;
     }
 
-    public static void Guess(bool isHigher) { }
+    public static void Guess(bool isHigher) {
+        int nextNumber = Random.Next(Minimum, Maximum + 1);
+
+        if (isHigher && nextNumber >= _currentNumber || !isHigher && nextNumber <= _currentNumber) {
+            Console.WriteLine("You guessed right!");
+            _pot++;
+        }
+        else {
+            Console.WriteLine("Bad luck, you guessed wrong!");
+            _pot--;
+        }
+
+        _currentNumber = nextNumber;
+        Console.WriteLine($"The current number is {_currentNumber}.");
+    }
 
     public static int GetPot() {
         return _pot;
     }
 
     public static void Hint() {
+        const int half = Maximum / 2;
+
+        Console.WriteLine(_currentNumber >= half
+            ? $"The number is at least {half}."
+            : $"The number is at most {half}.");
+
         _pot--;
     }
 }
--- BookHeadFirst/Chapter005/HighLowGame/HighLowGame/Program.cs
+++ BookHeadFirst/Chapter005/HighLowGame/HighLowGame/Program.cs
@@ -4,14 +4,14 @@ namespace HighLowGame;
 
 public static class Program {
     public static void Main(string[] args) {
-        Console.WriteLine("Welcome to the HiLo Game.");
+        Console.WriteLine("Welcome to the High Low Game.");
         Console.WriteLine($"Guess numbers between {HiLoGame.Minimum} and {HiLoGame.Maximum}.");
 
         HiLoGame.Hint();
 
         while (HiLoGame.GetPot() > 0) {
-            Console.WriteLine("Press h for higher, l for lower, ? to buy a hint,");
-            Console.WriteLine($"or any other key to quit with {HiLoGame.GetPot()}.");
+            Console.WriteLine(
+                $"Press h for higher, l for lower, ? to buy a hint, or any other key to quit with {HiLoGame.GetPot()}.");
             char choice = Console.ReadKey(true).KeyChar;
 
             switch (char.ToLower(choice)) {
