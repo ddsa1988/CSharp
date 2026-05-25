a2edd927cdfc7b6aeac7ae25c5220fca7cf49e08
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-24 22:45:45 -0300

    commit

--- BookHeadFirst/Chapter005/CalculateDamageApp/CalculateDamageApp/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter005/CalculateDamageApp/CalculateDamageApp/MainWindow.axaml.cs
@@ -1,9 +1,22 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CalculateDamageApp.Models;
 
 namespace CalculateDamageApp;
 
 public partial class MainWindow : Window {
+    private readonly SwordDamage _swordDamage = new();
+
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e) {
+        _swordDamage.RollDices();
+        _swordDamage.SetFlaming(Flaming?.IsChecked ?? false);
+        _swordDamage.SetMagic(Magic?.IsChecked ?? false);
+        _swordDamage.CalculateDamage();
+
+        Damage.Text = $"Rolled {_swordDamage.Roll} for {_swordDamage.Damage} HP";
     }
 }
