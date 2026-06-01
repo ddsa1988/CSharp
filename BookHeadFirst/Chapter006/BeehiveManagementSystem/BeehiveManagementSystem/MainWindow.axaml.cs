9d4b6c0c53e404cebcff9f48781639a20fddd9d6
Diego Alexandre <ddsa.1988@outlook.com>
2026-05-31 21:26:46 -0300

    commit

---
+++ BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace BeehiveManagementSystem;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void AssignJobClick(object? sender, RoutedEventArgs e) {
+        throw new System.NotImplementedException();
+    }
+
+    private void WorkShiftClick(object? sender, RoutedEventArgs e) {
+        throw new System.NotImplementedException();
+    }
+}
