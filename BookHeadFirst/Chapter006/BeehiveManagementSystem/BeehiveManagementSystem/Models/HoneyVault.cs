f02b843f6b2fb4980bc697b8d211452da8a5c97b
Diego Alexandre <ddsa1988@outlook.com>
2026-06-01 15:38:35 -0300

    commit

--- BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/Models/HoneyVault.cs
+++ BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/Models/HoneyVault.cs
@@ -5,8 +5,8 @@ namespace BeehiveManagementSystem.Models;
 public static class HoneyVault {
     private static float _honey = 25f;
     private static float _nectar = 100f;
-    public const float HoneyConversionRate = 0.19f;
-    public const float LowLevelWarning = 10f;
+    private const float HoneyConversionRate = 0.19f;
+    private const float LowLevelWarning = 10f;
     public static string StatusReport { get; } = string.Empty;
 
     public static void CollectNectar(float amount) {
@@ -14,7 +14,13 @@ public static class HoneyVault {
     }
 
     public static void ConvertNectarToHoney(float amount) {
-        throw new NotImplementedException();
+        if (amount <= 0) return;
+        if (_nectar <= 0) return;
+
+        float nectarAmount = amount < _nectar ? amount : _nectar;
+
+        _honey += nectarAmount * HoneyConversionRate;
+        _nectar -= nectarAmount;
     }
 
     public static bool ConsumeHoney(float amount) {
