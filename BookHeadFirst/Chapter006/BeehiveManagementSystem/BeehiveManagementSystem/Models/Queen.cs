39b6a6638a303aa46a772c05f32344dcb5bc56c7
Diego Alexandre <ddsa1988@outlook.com>
2026-06-02 14:37:26 -0300

    commit

--- BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/Models/Queen.cs
+++ BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/Models/Queen.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using BeehiveManagementSystem.Enums;
 
 namespace BeehiveManagementSystem.Models;
@@ -9,10 +10,34 @@ public class Queen : Bee {
     private const float HoneyPerUnassignedWorker = 0.5f;
     private readonly List<Bee> _workers = [];
     private float _eggs;
-    private int _unassignedWorkers = 3;
+    private float _unassignedWorkers;
+    public string StatusReport { get; private set; } = string.Empty;
     public override float CostPerShift { get; init; } = 2.15f;
 
-    public Queen() : base(Job.Queen) { }
+    public Queen() : base(Job.Queen) {
+        _unassignedWorkers = 3;
+
+        AssignBee(Job.NectarCollector);
+        AssignBee(Job.HoneyManufacturer);
+        AssignBee(Job.EggCare);
+    }
+
+    private void UpdateStatusReport() {
+        StatusReport = HoneyVault.StatusReport + '\n';
+        StatusReport += $"\nEgg count: {_eggs:0.0}";
+        StatusReport += $"\nUnassigned workers: {_unassignedWorkers:0.0}";
+        StatusReport += $"\n{WorkerStatus(Job.NectarCollector)}";
+        StatusReport += $"\n{WorkerStatus(Job.HoneyManufacturer)}";
+        StatusReport += $"\n{WorkerStatus(Job.EggCare)}";
+        StatusReport += $"\nTOTAL WORKERS: {_workers.Count}";
+    }
+
+    private string WorkerStatus(Job job) {
+        int count = _workers.Count(bee => bee.Job == job);
+        string status = $"{count} {nameof(job)} bee{(count > 2 ? "s" : "")}";
+
+        return status;
+    }
 
     private void AddWorker(Bee worker) {
         if (_unassignedWorkers < 1) return;
@@ -38,7 +63,21 @@ public class Queen : Bee {
         }
     }
 
+    public void CareForEggs(float eggsToConvert) {
+        if (eggsToConvert <= 0) return;
+        if (eggsToConvert > _eggs) return;
+
+        _eggs -= eggsToConvert;
+        _unassignedWorkers += eggsToConvert;
+    }
+
     protected override void DoJob() {
-        throw new NotImplementedException();
+        _eggs += EggsPerShit;
+
+        foreach (Bee worker in _workers) {
+            worker.WorkNextShift();
+        }
+
+        HoneyVault.ConsumeHoney(_unassignedWorkers * HoneyPerUnassignedWorker);
     }
 }
