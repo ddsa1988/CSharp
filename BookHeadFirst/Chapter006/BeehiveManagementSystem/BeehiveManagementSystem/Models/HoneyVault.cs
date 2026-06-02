90472e7f4fc45563ae57122798955d3974db5451
Diego Alexandre <ddsa.1988@outlook.com>
2026-06-01 23:13:33 -0300

    commit

--- BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/Models/HoneyVault.cs
+++ BookHeadFirst/Chapter006/BeehiveManagementSystem/BeehiveManagementSystem/Models/HoneyVault.cs
@@ -1,29 +1,50 @@
-using System;
-
 namespace BeehiveManagementSystem.Models;
 
 public static class HoneyVault {
-    private static float _honey = 25f;
-    private static float _nectar = 100f;
     private const float HoneyConversionRate = 0.19f;
     private const float LowLevelWarning = 10f;
-    public static string StatusReport { get; } = string.Empty;
+    public static float Honey { get; private set; } = 25f;
+    public static float Nectar { get; private set; } = 100f;
+
+    public static string StatusReport {
+        get {
+            string status = $"Vault report:\n{Honey:0.0} units of honey\n{Nectar:0.0} units of nectar";
+            string warnings = "";
+
+            if (Honey < LowLevelWarning) {
+                warnings += "\nLOW HONEY - ADD A HONEY MANUFACTURER";
+            }
+
+            if (Nectar < LowLevelWarning) {
+                warnings += "\nLOW NECTAR - ADD A NECTAR COLLECTOR";
+            }
+
+            return status + warnings;
+        }
+    }
 
     public static void CollectNectar(float amount) {
-        throw new NotImplementedException();
+        if (amount <= 0) return;
+
+        Nectar += amount;
     }
 
     public static void ConvertNectarToHoney(float amount) {
         if (amount <= 0) return;
-        if (_nectar <= 0) return;
+        if (Nectar <= 0) return;
 
-        float nectarAmount = amount < _nectar ? amount : _nectar;
+        float nectarAmount = amount < Nectar ? amount : Nectar;
 
-        _honey += nectarAmount * HoneyConversionRate;
-        _nectar -= nectarAmount;
+        Honey += nectarAmount * HoneyConversionRate;
+        Nectar -= nectarAmount;
     }
 
     public static bool ConsumeHoney(float amount) {
-        throw new NotImplementedException();
+        if (amount <= 0) return false;
+        if (amount > Honey) return false;
+
+        Honey -= amount;
+
+        return true;
     }
 }
