f32ad01229dfd2c313926a91aa8206532f35f0ca
Diego Alexandre <ddsa.1988@outlook.com>
2026-06-10 23:03:01 -0300

    commit

--- BookHeadFirst/Chapter008/TwoDecks/TwoDecks/MainWindow.axaml.cs
+++ BookHeadFirst/Chapter008/TwoDecks/TwoDecks/MainWindow.axaml.cs
@@ -1,9 +1,26 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TwoDecks;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void Button_Shuffle(object? sender, RoutedEventArgs e) {
+        throw new System.NotImplementedException();
+    }
+
+    private void Button_Reset(object? sender, RoutedEventArgs e) {
+        throw new System.NotImplementedException();
+    }
+
+    private void Button_Clear(object? sender, RoutedEventArgs e) {
+        throw new System.NotImplementedException();
+    }
+
+    private void Button_Sort(object? sender, RoutedEventArgs e) {
+        throw new System.NotImplementedException();
     }
 }
