d557e9ca3f8dbf20beeaff5e0e6d56778bc203e1
Diego Alexandre <ddsa1988@outlook.com>
2026-06-12 15:08:25 -0300

    commit

---
+++ BookHeadFirst/Chapter009/LinqQueries/LinqQueries/Examples/LinqTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqQueries.Examples;
+
+public static class LinqTest {
+    public static void Run() {
+        List<int> numbers = Enumerable.Range(1, 100).ToList();
+
+        IEnumerable<int> firstFiveAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+
+        Console.WriteLine(string.Join(", ", firstFiveAndLastFive));
+    }
+}
---
+++ BookHeadFirst/Chapter009/LinqQueries/LinqQueries/Program.cs
@@ -0,0 +1,9 @@
+﻿using LinqQueries.Examples;
+
+namespace LinqQueries;
+
+public static class Program {
+    public static void Main(string[] args) {
+        LinqTest.Run();
+    }
+}
