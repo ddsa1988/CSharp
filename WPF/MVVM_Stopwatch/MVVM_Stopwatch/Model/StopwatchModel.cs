4761975f8232bd2646f4a48a3f03a5ec4dcfab7d
Diego Alexandre <ddsa1988@outlook.com>
2026-07-17 07:38:44 -0300

    commit

--- WPF/MVVM_Stopwatch/MVVM_Stopwatch/Model/StopwatchModel.cs
+++ WPF/MVVM_Stopwatch/MVVM_Stopwatch/Model/StopwatchModel.cs
@@ -1,3 +1,33 @@
 ﻿namespace MVVM_Stopwatch.Model;
 
-public class StopwatchModel { }
+public class StopwatchModel {
+    private DateTime _startedTime;
+
+    /// <summary>
+    /// The constructor reset the stopwatch
+    /// </summary>
+    public StopwatchModel() {
+        Reset();
+    }
+
+    /// <summary>
+    /// Returns true if the stopwatch is running
+    /// </summary>
+    public bool Running {
+        get => _startedTime != DateTime.MinValue;
+        set {
+            if (!value || Running) return;
+            _startedTime = DateTime.Now;
+        }
+    }
+
+    /// <summary>
+    /// Returns the elapsed time, or zero if the stop watch is not running
+    /// </summary>
+    public TimeSpan ElapsedTime => Running ? DateTime.Now - _startedTime : TimeSpan.Zero;
+
+    /// <summary>
+    /// Resets the stopwatch by setting its started time to DateTime.MinValue
+    /// </summary>
+    public void Reset() => _startedTime = DateTime.MinValue;
+}
