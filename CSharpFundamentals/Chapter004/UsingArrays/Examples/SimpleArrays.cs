07b48d9e2cffa9be6a8f51aa7821553d60030854
Diego Alexandre <ddsa.1988@outlook.com>
2026-08-18 22:48:05 -0300

    commit

---
+++ CSharpFundamentals/Chapter004/UsingArrays/Examples/SimpleArrays.cs
@@ -0,0 +1,10 @@
+namespace UsingArrays.Examples;
+
+internal static class SimpleArrays {
+    internal static void Run() {
+        Console.WriteLine("=> Simple Array Creation:\n");
+
+        int[] myInts = new int[5];
+        string[] myStrings = new string[5];
+    }
+}
---
+++ CSharpFundamentals/Chapter004/UsingArrays/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace UsingArrays;
+
+public static class Program {
+    public static void Main(string[] args) {
+        Examples.SimpleArrays.Run();
+    }
+}
