7648e23170803b3e4a03dec6e86ec73b2116d11c
Diego Alexandre <ddsa1988@outlook.com>
2026-09-16 07:48:09 -0300

    commit

---
+++ CSharpFundamentals/Chapter009/SimpleGC/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleGC.Models;
+
+internal class Car:IDisposable {
+    public int CurrentSpeed { get; set; }
+    public string Name { get; set; }
+
+    public Car(string name, int speed) {
+        Name = name;
+        CurrentSpeed = speed;
+    }
+    
+    public Car():this(string.Empty, 0) { }
+
+    public override string ToString() {
+        return $"Car {{ {nameof(Name)} = {Name}, {nameof(CurrentSpeed)} = {CurrentSpeed} }}";
+    }
+
+    // The object user should call this method when they finish with the object
+    public void Dispose() {
+        // TODO release managed resources here
+        Console.WriteLine("***** In Dispose! *****");
+    }
+}
+
