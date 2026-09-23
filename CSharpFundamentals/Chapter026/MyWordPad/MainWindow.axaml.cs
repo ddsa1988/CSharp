973c5bacba35adec7c861bdb015663cccb9d7859
Diego Alexandre <ddsa.1988@outlook.com>
2026-09-22 22:52:20 -0300

    commit

--- CSharpFundamentals/Chapter026/MyWordPad/MainWindow.axaml.cs
+++ CSharpFundamentals/Chapter026/MyWordPad/MainWindow.axaml.cs
@@ -1,25 +1,45 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.SpellChecker;
 
 namespace MyWordPad;
 
 public partial class MainWindow : Window {
+    private readonly TextBoxSpellChecker _spellChecker;
+
     public MainWindow() {
         InitializeComponent();
+
+        _spellChecker = new TextBoxSpellChecker(SpellCheckerConfig.Create("pt_BR", "en_GB"));
+        var textBox = this.FindControl<TextBox>("TxtData");
+
+        if (textBox == null) return;
+
+        _spellChecker.Initialize(textBox);
     }
 
-    private void ExitArea_OnPointerEntered(object? sender, PointerEventArgs e) { }
+    private void ExitArea_OnPointerEntered(object? sender, PointerEventArgs e) {
+        StatBarText.Text = "Exit the Application";
+    }
 
-    private void ExitArea_OnPointerExited(object? sender, PointerEventArgs e) { }
+    private void ExitArea_OnPointerExited(object? sender, PointerEventArgs e) {
+        StatBarText.Text = "Ready";
+    }
 
     private void FileExit_OnClick(object? sender, RoutedEventArgs e) {
         Close();
     }
 
-    private void ToolsHintsArea_OnPointerEntered(object? sender, PointerEventArgs e) { }
+    private void ToolsHintsArea_OnPointerEntered(object? sender, PointerEventArgs e) {
+        StatBarText.Text = "Show Spelling Suggestions";
+    }
 
     private void ToolsHintsArea_OnPointerExited(object? sender, PointerEventArgs e) { }
 
-    private void ToolsSpellingHints_OnClick(object? sender, RoutedEventArgs e) { }
+    private void ToolsSpellingHints_OnClick(object? sender, RoutedEventArgs e) {
+        string spellingHints = string.Empty;
+
+        Console.WriteLine(TxtData.CaretIndex);
+    }
 }
